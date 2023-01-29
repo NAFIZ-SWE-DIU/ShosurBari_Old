256dc57158825ffa87cb9251e6ce1f68e62582a9
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-01-29 22:16:44 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Find Your Perfect Partner - code-projects.org | Home :: 
-</title>
+<title>Home - ShosurBari</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
@@ -50,87 +49,30 @@ $(document).ready(function(){
 
 
 
-<div class="grid_1">
+
 
 
 
 
 <form action="#" id="header-search-people" class="form-area" novalidate="novalidate" autocomplete="off">
+
+	<div class="Findpartner">
+		<h1>Find your perfect life partner</h1>
+</div>
+
   <div class="row">
       <div class="col-md-10">
           <div class="styled-input wide multi">
-              <div class="first-name" id="input-first-name">
-                  <input type="text" name="fn" id="fn" autocomplete="off" data-placeholder-focus="false" required />
-                  <label>First Name</label>
-                  <svg class="icon--check" width="21px" height="17px" viewBox="0 0 21 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
-                          <g id="UI-Elements-Forms" transform="translate(-255.000000, -746.000000)" fill-rule="nonzero" stroke="#81B44C" stroke-width="3">
-                              <polyline id="Path-2" points="257 754.064225 263.505943 760.733489 273.634603 748"></polyline>
-                          </g>
-                      </g>
-                  </svg>
-                  <svg class="icon--error" width="15px" height="15px" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
-                          <g id="UI-Elements-Forms" transform="translate(-550.000000, -747.000000)" fill-rule="nonzero" stroke="#D0021B" stroke-width="3">
-                              <g id="Group" transform="translate(552.000000, 749.000000)">
-                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy"></path>
-                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy-2" transform="translate(5.564949, 5.564949) scale(-1, 1) translate(-5.564949, -5.564949) "></path>
-                              </g>
-                          </g>
-                      </g>
-                  </svg>
-              </div>
-              <div class="last-name" id="input-last-name">
-                  <input type="text" name="ln" id="ln" autocomplete="off" data-placeholder-focus="false" required />
-                  <label>Last Name</label>
-                  <svg class="icon--check" width="21px" height="17px" viewBox="0 0 21 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
-                          <g id="UI-Elements-Forms" transform="translate(-255.000000, -746.000000)" fill-rule="nonzero" stroke="#81B44C" stroke-width="3">
-                              <polyline id="Path-2" points="257 754.064225 263.505943 760.733489 273.634603 748"></polyline>
-                          </g>
-                      </g>
-                  </svg>
-                  <svg class="icon--error" width="15px" height="15px" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
-                          <g id="UI-Elements-Forms" transform="translate(-550.000000, -747.000000)" fill-rule="nonzero" stroke="#D0021B" stroke-width="3">
-                              <g id="Group" transform="translate(552.000000, 749.000000)">
-                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy"></path>
-                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy-2" transform="translate(5.564949, 5.564949) scale(-1, 1) translate(-5.564949, -5.564949) "></path>
-                              </g>
-                          </g>
-                      </g>
-                  </svg>
-              </div>
-              <div class="city" id="input-city">
-                  <input type="text" name="city" id="city" autocomplete="off" data-placeholder-focus="false" />
-                  <label>City</label>
-                  <svg class="icon--check" width="21px" height="17px" viewBox="0 0 21 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
-                          <g id="UI-Elements-Forms" transform="translate(-255.000000, -746.000000)" fill-rule="nonzero" stroke="#81B44C" stroke-width="3">
-                              <polyline id="Path-2" points="257 754.064225 263.505943 760.733489 273.634603 748"></polyline>
-                          </g>
-                      </g>
-                  </svg>
-                  <svg class="icon--error" width="15px" height="15px" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
-                          <g id="UI-Elements-Forms" transform="translate(-550.000000, -747.000000)" fill-rule="nonzero" stroke="#D0021B" stroke-width="3">
-                              <g id="Group" transform="translate(552.000000, 749.000000)">
-                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy"></path>
-                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy-2" transform="translate(5.564949, 5.564949) scale(-1, 1) translate(-5.564949, -5.564949) "></path>
-                              </g>
-                          </g>
-                      </g>
-                  </svg>
-              </div>
+
+
+			  
               <div class="state" id="select-state">
                   <select name="state">
                       <option value="All">All</option>
-                      <option value="FL">Florida</option>
-                      <option value="GA">Georgia</option>
-                      <option value="HI">Hawaii</option>
-                      <option value="ID">Idaho</option>
+                      <option value="AL">Groom</option>
+                      <option value="AK">Bride</option>
                   </select>
-                  <label>State</label>
+                  <label>Groom or Bride</label>
                   <svg class="chevron-down" width="17px" height="10px" viewBox="0 0 17 10" version="1.1" xmlns="http://www.w3.org/2000/svg"
                       xmlns:xlink="http://www.w3.org/1999/xlink">
                       <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -143,14 +85,114 @@ $(document).ready(function(){
                       </g>
                   </svg>
               </div>
+
+              <div class="state" id="select-state">
+                  <select name="state">
+                      <option value="All">All</option>
+                      <option value="AL">Rajshahi</option>
+                      <option value="AK">Dhaka</option>
+                      <option value="AZ">Sylhet</option>
+                      <option value="AR">Khulna</option>
+                      <option value="CA">Chittagong</option>
+                      <option value="CO">Rangpur</option>
+					  <option value="CA">Barishal</option>
+                      <option value="CO">Mymenshing</option>
+                  </select>
+                  <label>District</label>
+                  <svg class="chevron-down" width="17px" height="10px" viewBox="0 0 17 10" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="UI-Elements-Forms" transform="translate(-840.000000, -753.000000)" stroke="#4A4A4A" stroke-width="0.9">
+                              <g id="Done-Copy-2" transform="translate(622.000000, 727.000000)">
+                                  <polyline id="Rectangle-16" transform="translate(226.400000, 27.400000) rotate(-45.000000) translate(-226.400000, -27.400000) "
+                                      points="231.8 32.8 221 32.8 221 22"></polyline>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+              </div>
+
+              <div class="state" id="select-state">
+                  <select name="state">
+                      <option value="All">All</option>
+                      <option value="AL">Unmarriade</option>
+                      <option value="AK">Divorce</option>
+                      <option value="AZ">Bidhoba</option>
+                      <option value="AR">Bipotnik</option>
+
+                  </select>
+                  <label>Maritial Status</label>
+                  <svg class="chevron-down" width="17px" height="10px" viewBox="0 0 17 10" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="UI-Elements-Forms" transform="translate(-840.000000, -753.000000)" stroke="#4A4A4A" stroke-width="0.9">
+                              <g id="Done-Copy-2" transform="translate(622.000000, 727.000000)">
+                                  <polyline id="Rectangle-16" transform="translate(226.400000, 27.400000) rotate(-45.000000) translate(-226.400000, -27.400000) "
+                                      points="231.8 32.8 221 32.8 221 22"></polyline>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+              </div>
+
+			  <div class="state" id="select-state">
+                  <select name="state">
+                      <option value="All">All</option>
+                      <option value="AL">Muslim</option>
+                      <option value="AK">Hindusm</option>
+                      <option value="AZ">Christian</option>
+                      <option value="AR">Budhism</option>
+                  </select>
+                  <label>Religion</label>
+                  <svg class="chevron-down" width="17px" height="10px" viewBox="0 0 17 10" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                          <g id="UI-Elements-Forms" transform="translate(-840.000000, -753.000000)" stroke="#4A4A4A" stroke-width="0.9">
+                              <g id="Done-Copy-2" transform="translate(622.000000, 727.000000)">
+                                  <polyline id="Rectangle-16" transform="translate(226.400000, 27.400000) rotate(-45.000000) translate(-226.400000, -27.400000) "
+                                      points="231.8 32.8 221 32.8 221 22"></polyline>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+              </div>
+
+			  <div class="first-name" id="input-first-name">
+				  <input type="text" value="" id="profid" name="profid" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = '';}" autocomplete="on" data-placeholder-focus="false" required />
+                  <label>Biodata Number</label>
+          <script type="text/javascript">
+            function viewprofile(){
+              var profid=document.getElementById("profid").value;
+              window.location.href="view_profile.php?id="+profid;
+            }
+          </script>
+
+                  <svg class="icon--check" width="21px" height="17px" viewBox="0 0 21 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
+                          <g id="UI-Elements-Forms" transform="translate(-255.000000, -746.000000)" fill-rule="nonzero" stroke="#81B44C" stroke-width="3">
+                              <polyline id="Path-2" points="257 754.064225 263.505943 760.733489 273.634603 748"></polyline>
+                          </g>
+                      </g>
+                  </svg>
+                  <svg class="icon--error" width="15px" height="15px" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                      <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-linecap="round">
+                          <g id="UI-Elements-Forms" transform="translate(-550.000000, -747.000000)" fill-rule="nonzero" stroke="#D0021B" stroke-width="3">
+                              <g id="Group" transform="translate(552.000000, 749.000000)">
+                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy"></path>
+                                  <path d="M0,11.1298982 L11.1298982,-5.68434189e-14" id="Path-2-Copy-2" transform="translate(5.564949, 5.564949) scale(-1, 1) translate(-5.564949, -5.564949) "></path>
+                              </g>
+                          </g>
+                      </g>
+                  </svg>
+              </div>
+
           </div>
       </div>
-
       <div class="col-md-2 no-pad-left-10">
-          <button type="submit" class="primary-btn serach-btn" id="submit_btn">SEARCH</button>
-      </div>
+	      <button  type="button" target="_blank" class="primary-btn serach-btn" onclick="viewprofile();">SEARCH</button>
 
-    </div>
+      </div>
+  </div>
 </form>
 
 
@@ -161,6 +203,16 @@ $(document).ready(function(){
 
 
 
+
+
+
+
+
+
+
+
+
+<div class="grid_1">
       <div class="container">
       	<h1>Featured Profiles</h1>
        	<div class="heart-divider">
