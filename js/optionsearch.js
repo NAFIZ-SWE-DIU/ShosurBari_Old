7605eecc5f8199d956e340eb80c7213d96cb876c
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-02-20 05:46:20 +0600

    Update optionsearch.js

--- js/optionsearch.js
+++ js/optionsearch.js
@@ -26,3 +26,115 @@ function initClock() {
   window.setInterval("updateClock()", 1);
 }
 // END CLOCK SCRIPT
+
+
+/*
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!-- eye icon password show -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+
+		      <input type="password" id="edit-pass" placeholder="Enter Confirm Password" name="pass_2" size="60" maxlength="128" class="form-text required" required>
+			  <span class="show-password"><i id="show-pass" class="fa fa-eye" aria-hidden="true"></i></span>
+
+        
+		<script>
+      let showPass = document.getElementById('show-pass');
+      let editPass = document.getElementById('edit-pass');
+      showPass.addEventListener('click', function(){
+        if (editPass.type === 'password') {
+          editPass.type = 'text';
+          showPass.className = 'fa fa-eye-slash';
+        } else {
+          editPass.type = 'password';
+          showPass.className = 'fa fa-eye';
+        }
+      });
+    </script>
+ */
+
+
+
+
+    function validateForm(){
+      var fname = document.forms["myForm"]["fname"].value;
+      var uname = document.forms["myForm"]["uname"].value;
+      var email = document.forms["myForm"]["email"].value;
+      var pnumber = document.forms["myForm"]["pnumber"].value;
+      var pass_1 = document.forms["myForm"]["pass_1"].value;
+      var pass_2 = document.forms["myForm"]["pass_2"].value;
+      
+      //Full Name validation
+      if (fname == "") {
+        document.getElementById('fname').style.borderColor = "red";
+        document.getElementById('fname_error').innerHTML = "Please enter your Full Name";
+        return false;
+      }else{
+        document.getElementById('fname').style.borderColor = "green";
+        document.getElementById('fname_error').innerHTML = "";
+      }
+      
+      //Username validation
+      if (uname == "") {
+        document.getElementById('uname').style.borderColor = "red";
+        document.getElementById('uname_error').innerHTML = "Please enter your Username";
+        return false;
+      }else if (! /^[A-Za-z0-9]+$/.test(uname)){
+        document.getElementById('uname').style.borderColor = "red";
+        document.getElementById('uname_error').innerHTML = "Please enter only characters and numbers";
+        return false;
+      }else{
+        document.getElementById('uname').style.borderColor = "green";
+        document.getElementById('uname_error').innerHTML = "";
+      }
+      
+      //Email validation
+      if (email == "") {
+        document.getElementById('email').style.borderColor = "red";
+        document.getElementById('email_error').innerHTML = "Please enter your Email";
+        return false;
+      }else if(! /^[a-zA-Z0-9._-]+@(gmail|outlook|hotmail|yahoo).com$/.test(email)){
+        document.getElementById('email').style.borderColor = "red";
+        document.getElementById('email_error').innerHTML = "Please enter a valid Email";
+        return false;
+      }else{
+        document.getElementById('email').style.borderColor = "green";
+        document.getElementById('email_error').innerHTML = "";
+      }
+      
+      //Phone number validation
+      if (pnumber == "") {
+        document.getElementById('pnumber').style.borderColor = "red";
+        document.getElementById('pnumber_error').innerHTML = "Please enter your Phone Number";
+        return false;
+      }else if(pnumber.length < 10 || pnumber.length > 14){
+        document.getElementById('pnumber').style.borderColor = "red";
+        document.getElementById('pnumber_error').innerHTML = "Phone number must be between 10 and 14 characters";
+        return false;
+      }else{
+        document.getElementById('pnumber').style.borderColor = "green";
+        document.getElementById('pnumber_error').innerHTML = "";
+      }
+      
+      //Password validation
+      if (pass_1 == "") {
+        document.getElementById('pass_1').style.borderColor = "red";
+        document.getElementById('pass_1_error').innerHTML = "Please enter your Password";
+        return false;
+      }else{
+        document.getElementById('pass_1').style.borderColor = "green";
+        document.getElementById('pass_1_error').innerHTML = "";
+      }
+      
+      //Confirm Password validation
+      if (pass_2 == "") {
+        document.getElementById('pass_2').style.borderColor = "red";
+        document.getElementById('pass_2_error').innerHTML = "Please enter your Confirm Password";
+        return false;
+      }else if(pass_2 != pass_1){
+        document.getElementById('pass_2').style.borderColor = "red";
+        document.getElementById('pass_2_error').innerHTML = "Password do not match";
+        return false;
+      }else{
+        document.getElementById('pass_2').style.borderColor = "green";
+        document.getElementById('pass_2_error').innerHTML = "";
+      }
+    }
