557afc084612989810c461cdaa5e45e954890673
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-02-21 05:32:42 +0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -46,8 +46,8 @@ $(document).ready(function(){
    <div class="breadcrumb1">
      <ul>
         <a href="index.php"><i class="fa fa-home home_1"></i></a>
-        <span class="divider">&nbsp;|&nbsp;</span>
-        <li class="current-page">Contact</li>
+        <span class="divider">&nbsp;<|>&nbsp;</span>
+        <li class="current-page"><h4>Contact</h4></li>
      </ul>
    </div>
    <div class="grid_5">
