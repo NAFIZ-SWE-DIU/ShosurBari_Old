d696447a0e99652eacbe74801d267b6b27ca9269
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-02-21 05:31:13 +0600

    Update services.php

--- services.php
+++ services.php
@@ -46,8 +46,8 @@ $(document).ready(function(){
    <div class="breadcrumb1">
      <ul>
         <a href="index.php"><i class="fa fa-home home_1"></i></a>
-        <span class="divider">&nbsp;|&nbsp;</span>
-        <li class="current-page">Services</li>
+        <span class="divider">&nbsp;<|>&nbsp;</span>
+        <li class="current-page"><h4>Services</h4></li>
      </ul>
    </div>
    <div class="services">
