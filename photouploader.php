4f897423b6fb3133e4772df48614be98412308a2
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-02-28 02:36:13 +0600

    Update photouploader.php

--- photouploader.php
+++ photouploader.php
@@ -55,38 +55,55 @@ $(document).ready(function(){
    <div class="breadcrumb1">
      <ul>
         <a href="index.php"><i class="fa fa-home home_1"></i></a>
-        <span class="divider">&nbsp;|&nbsp;</span>
-        <li class="current-page">Login</li>
+        <span class="divider">&nbsp;<|>&nbsp;</span>
+        <li class="current-page"><h4>Photo Upload</h4></li>
      </ul>
    </div>
+   
+   
+
+
+
+
+
+
+
    <div class="services">
    	  <div class="col-sm-6 login_left">
 	   <form action="" method="post" enctype="multipart/form-data">
   	    <div class="form-item form-type-textfield form-item-name">
-	      <label for="edit-name">Upload Your Photo(Upto 4 images, Use 300 x 250 dimensions) <span class="form-required" title="This field is required.">*</span></label>
-	      <input type="file" id="edit-name" name="pic1" value="<?php if (isset($_POST['pic1'])) echo $_POST['pic1']; ?>" class="form-file required">
+	      <label for="edit-name">Upload Your one Photo, Use 300 x 250 dimensions. <span class="form-required" title="This field is required.">*</span></label>
+	      <input type="file" id="edit-name" name="pic1" value="<?php
+if(isset($_POST['submit'])){
+    $user_id = $_POST['user_id'];
+
+    if(isset($_FILES['pic1'])){
+        //get file details
+        $file_name = $_FILES['pic1']['name'];
+
+        //show file name
+        echo "File Name: ".$file_name;
+    }
+}
+?>     " class="form-file required">
 	    </div>
 	    <div class="form-actions">
 	    	<input type="submit" id="edit-submit" name="op" value="Upload" class="btn_1 submit">
 	    </div>
+
+
+
 	   </form>
-	  </div>
-	  <div class="col-sm-6">
-	    <ul class="sharing">
-			<li><a href="#" class="facebook" title="Facebook"><i class="fa fa-boxed fa-fw fa-facebook"></i> Share on Facebook</a></li>
-		  	<li><a href="#" class="twitter" title="Twitter"><i class="fa fa-boxed fa-fw fa-twitter"></i> Tweet</a></li>
-		  	<li><a href="#" class="google" title="Google"><i class="fa fa-boxed fa-fw fa-google-plus"></i> Share on Google+</a></li>
-		  	<li><a href="#" class="linkedin" title="Linkedin"><i class="fa fa-boxed fa-fw fa-linkedin"></i> Share on LinkedIn</a></li>
-		  	<li><a href="#" class="mail" title="Email"><i class="fa fa-boxed fa-fw fa-envelope-o"></i> E-mail</a></li>
-		</ul>
 	  </div>
 	  <div class="clearfix"> </div>
    </div>
   </div>
 </div>
 
-
 <?php include_once("footer.php");?>
 
 </body>
 </html>	
+
+
+
