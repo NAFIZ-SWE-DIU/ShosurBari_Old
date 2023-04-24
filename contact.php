28f74a02742b27a3388b83660cc4049171698ce3
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-04-25 04:12:39 +0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -323,57 +323,289 @@ text-align: center;
 
 
 
-<div class="about_middle">
-<div class="container">
   <div class="shosurbari-biodata">
+  <form action="" method="POST" name="myForm" onsubmit="return validateForm()">
+    <div class="flex-container">
+      <div class="sb-register-login">
+        <div class="sb-biodata-field" style="background: #00bbff !important;">
+          <h2>Contact <span>Us</span></h2>
+        </div>
 
-    <form action="" method="POST">
-	  <div class="flex-container">
-    <div class="sb-register-login">
+        <div class="shosurbari-biodata-field">
+          <input type="text" id="cust_name" placeholder="Your Full Name" name="name_contactus" value="" size="60" maxlength="60" class="form-text required">
+          <span id="name-error" style="color:red; font-size:13px;"></span>
+        </div>
 
-		  <div class="sb-biodata-field" style="background: #00bbff !important;">
-		    <h2>Contact <span>Form</span></h2>
-      </div>
+        <div class="shosurbari-biodata-field">
+          <input type="email" id="cust_email" placeholder="Your Email" name="email_contactus" value="" size="60" maxlength="60" class="form-text">
+          <span id="email-error" style="color:red; font-size:13px;"></span>
+        </div>
 
-      <div class="shosurbari-biodata-field">
-		    <input type="text" id="name_contactus" placeholder="Your Full Name*" name="name_contactus" value="" size="60" maxlength="60" class="form-text required" required>
-		  </div>
+        <div class="shosurbari-biodata-field">
+          <input type="tel" id="pnumber" placeholder="Your Phone Number" name="number_contactus" value="" size="60" minlength="10" maxlength="15" class="form-text required">
+          <span id="phone-error" style="color:red; font-size:13px;"></span>
+        </div>
 
-      <div class="shosurbari-biodata-field">
-		    <input type="number_contactus" id="number_contactus" placeholder="Your Number*" name="number_contactus" value="" size="60" maxlength="60" class="form-text required" required>
-			</div>
-
-      <script>
-      $(document).ready(function() {
-      var input = document.querySelector("#number_contactus");
+  <script>
+    $(document).ready(function() {
+      var input = document.querySelector("#pnumber");
       window.intlTelInput(input, {
         separateDialCode: true,
         preferredCountries: ["bd"]
       });
-      });
-      </script>
+    });
+  </script>
 
 
-      <div class="shosurbari-biodata-field">
-	      <input type="text" id="email_contactus" name="email_contactus" placeholder="Email (Optional)" value="" size="100" maxlength="100" class="form-text required">
+        <div class="shosurbari-biodata-field">
+          <textarea rows="4" id="contact_biodatas_number" name="message_contactus" placeholder="Type text..." class="form-text-describe required"></textarea>
+          <span id="biodata-error" style="color:red; font-size:13px;"></span>
+        </div>
+
+        <div class="form-actions">
+          <button type="submit" id="edit-submit" name="op" class="btn_4 submit">Submit</button>
+        </div>
+
+        <!-- Add this code after the form element in your existing code -->
+
+<!-- Popup message -->
+<div class="popup-message">
+  <h3></h3>
+  <p></p>
+</div>
+
+<!-- Overlay -->
+<div class="overlay"></div>
+
+
+
       </div>
-
-      <div class="shosurbari-biodata-field">
-				<textarea rows="4" id="message_contactus" name="message_contactus"  placeholder="Write Message*..." class="form-text-describe" required></textarea>
-		  </div>
-
-			<div class="form-actions">
-			  <button  type="submit" id="submit" name="submit" class="btn_4 submit">Submit</button>
-			</div>
-
     </div>
-    </div>
-	  </form>
-
-  </div>
-</div>
+  </form>
 </div>
 
+<script>
+  function validateForm() {
+  var name = document.getElementById("cust_name").value.trim();
+  var email = document.getElementById("cust_email").value.trim();
+  var phone = document.getElementById("pnumber").value.trim();
+  var biodata = document.getElementById("contact_biodatas_number").value.trim();
+  var nameError = document.getElementById("name-error");
+  var emailError = document.getElementById("email-error");
+  var phoneError = document.getElementById("phone-error");
+  var biodataError = document.getElementById("biodata-error");
+  var valid = true;
+
+  // Validate name
+
+
+
+//Full Name validation
+        if (name == "") {
+        document.getElementById('cust_name').style.borderColor = "red";
+        document.getElementById('cust_name').scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        document.getElementById('name-error').innerHTML = "Please enter your Full Name";
+        return false;
+      }else{
+        document.getElementById('cust_name').style.borderColor = "green";
+        document.getElementById('name-error').innerHTML = "";
+      }
+
+
+            //Email validation
+            if (email == "") {
+        document.getElementById('cust_email').style.borderColor = "red";
+        document.getElementById('cust_email').scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        document.getElementById('email-error').innerHTML = "Please enter your Email";
+        return false;
+      }else if(! /^[a-zA-Z0-9._-]+@(gmail|outlook|hotmail|yahoo).com$/.test(email)){
+        document.getElementById('cust_email').style.borderColor = "red";
+        document.getElementById('email-error').innerHTML = "Please enter a valid Email. Ex: (@gmail / @outlook / @hotmail / @yahoo).com";
+        document.getElementById('cust_email').scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        return false;
+      }else{
+        document.getElementById('cust_email').style.borderColor = "green";
+        document.getElementById('email-error').innerHTML = "";
+      }
+
+
+  //Phone number validation
+            if (phone == "") {
+        document.getElementById('pnumber').style.borderColor = "red";
+        document.getElementById('pnumber').scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        document.getElementById('phone-error').innerHTML = "Please enter your Phone Number";
+        return false;
+      }else if(pnumber.length < 10 || pnumber.length > 14){
+        document.getElementById('pnumber').style.borderColor = "red";
+        document.getElementById('phone-error').innerHTML = "Phone number must be between 10 and 14 characters";
+        document.getElementById('pnumber').scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        return false;
+      }else{
+        document.getElementById('pnumber').style.borderColor = "green";
+        document.getElementById('phone-error').innerHTML = "";
+      }
+
+  // Validate biodata
+
+  if (biodata == "") {
+        document.getElementById('contact_biodatas_number').style.borderColor = "red";
+        document.getElementById('contact_biodatas_number').scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+        });
+        document.getElementById('biodata-error').innerHTML = "Please type your text";
+        return false;
+      }else{
+        document.getElementById('contact_biodatas_number').style.borderColor = "green";
+        document.getElementById('biodata-error').innerHTML = "";
+      }
+
+  return valid;
+}
+  </script>
+
+
+
+
+
+
+
+
+
+
+<script> 
+function showSuccessMessage() {
+  // Show the overlay
+  document.querySelector('.overlay').style.display = 'block';
+
+  // Show the popup message
+  var popup = document.querySelector('.popup-message');
+  popup.style.display = 'block';
+
+  // Set the message text
+  popup.querySelector('h3').innerHTML = 'Thank you!';
+  popup.querySelector('p').innerHTML = 'Your data has been saved to the database.';
+
+  // Add a close button to the popup message
+  var closeButton = document.createElement('button');
+  closeButton.innerHTML = 'Close';
+  closeButton.classList.add('close-button');
+  popup.appendChild(closeButton);
+
+  // Hide the popup and overlay when the close button is clicked
+  closeButton.addEventListener('click', function() {
+    popup.style.display = 'none';
+    document.querySelector('.overlay').style.display = 'none';
+  });
+}
+
+// Change the form submission code to the following
+$('form[name="myForm"]').submit(function(e) {
+  e.preventDefault(); // Prevent the default form submission
+
+  if (validateForm()) {
+    // Submit the form data using AJAX
+    $.ajax({
+      url: 'contact.php', // Replace this with the URL of your server-side script
+      type: 'POST',
+      data: $(this).serialize(),
+      success: function(response) {
+        // Show the success message
+        showSuccessMessage();
+
+        // Clear the form
+        $('form[name="myForm"]')[0].reset();
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        // Handle errors here
+      }
+    });
+  }
+});
+</script>
+
+
+
+
+
+
+
+    
+<style>/* Popup message styles */
+.popup-message {
+  display: none;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #c5ffc5;
+  border: 2px solid green;
+  border-radius: 4px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  z-index: 9999;
+}
+
+.popup-message h3 {
+  margin-top: 0;
+  margin-bottom: 10px;
+  text-align: center;
+  color: #04aa6d;
+  font-size: 24px;
+}
+
+.popup-message p {
+  margin: 0;
+  font-size: 18px;
+  text-align: center;
+  color: #000;
+}
+
+.popup-message button.close-button {
+  display: block;
+  margin-top: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 5px 10px;
+  background-color: #00bbff;
+  color: #fff;
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+}
+
+.popup-message button.close-button:hover {
+  background-color: #ff0080;
+  color: #fff;
+}
+
+/* Overlay styles */
+.overlay {
+  display: none;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 9998;
+}
+</style>
 
 
 <?php include_once("footer.php");?>
