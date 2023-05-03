0543ce685f2416530b057cc41e6caeca370d686c
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-05-04 03:18:35 +0600

    Update 404.php

--- 404.php
+++ 404.php
@@ -7,124 +7,104 @@ $result=search();
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Search - ShosurBari</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="js/optionsearch.js"></script>
-<script src="js/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<!-- Custom Theme files -->
-<link href="css/style.css" rel='stylesheet' type='text/css' />
-<link href='//fonts.googleapis.com/css?family=Oswald:300,400,700' rel='stylesheet' type='text/css'>
-<link href='//fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
-<!--font-Awesome-->
-<link href="css/font-awesome.css" rel="stylesheet"> 
-<!--font-Awesome-->
-<script>
-$(document).ready(function(){
-    $(".dropdown").hover(            
-        function() {
-            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-            $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-            $(this).toggleClass('open');       
-        }
-    );
-});
-</script>
+<title>Error - ShosurBari</title>
 </head>
 <body>
-<!-- ============================  Navigation Start =========================== -->
- <?php include_once("includes/navigation.php");?>
-<!-- ============================  Navigation End ============================ -->
-
-
-
 
    
    
    
-   
-   <div class="error-page">
-		<div class="container">
-			<div class="row">
-					<div class="error-message">
-						<h1 class="error-title">Page Not Found</h1>
-						<h2 class="error-code">404 Error</h2>
-						<p class="error-text">The page you are searching for is not available.</p>
-						<a href="index.php" class="btn btn-primary">Go Back to Home Page</a>
-					</div>
-			</div>
-		</div>
-	</div>
+    <!-- 404 Page area Start-->
+    <div class="error-page-area">
+        <div class="error-page-wrap">
+            <i class="notika-icon notika-close"></i>
+            <h2>ERROR <span class="counter" style="color:#ff0080;">404!</span></h2>
+            <p>Sorry, But the page you are looking for has not been found. Try checking the URL for an error, then hit the refresh button on your browser or try found something else in our site.</p>
+            <a href="index.php" class="btn">Go to Back Home Page</a>
+            <a href="contact.php" class="btn error-btn-mg">Report Problem</a>
+        </div>
+    </div>
+    <!-- 404 Page area End-->
+ 
 
     <style>
-        /* Center the error message */
-.error-message {
-    text-align: center;
-    margin-top: 70px;
+.error-page-area{
+    min-height:100vh;
+    text-align:center;
+    background:#00bbff;
 }
-
-/* Add background color and padding to the error page */
-.error-page {
-    background-color: #f8f9fa;
-    padding: 20px 0;
-    text-align: center;
+.error-page-area:before{
+    content:'';
+    height:100vh;
+    width:1px;
 }
-
-/* Style the error title */
-.error-title {
-    font-size: 3rem;
-    color: red;
-}
-
-/* Style the error code */
-.error-code {
-    font-size: 7rem;
-    font-weight: bold;
-    color: #000;
-    margin-top: 10px;
-    margin-bottom: 10px;
-}
-
-/* Style the error text */
-.error-text {
-    font-size: 1.5rem;
-    margin-bottom: 30px;
-}
-
-/* Style the button */
-.btn-primary {
-    border-color: #007bff;
-    font-size: 1rem;
-    font-weight: bold;
-
-    background: #ff0080;
-    border-radius: 6px;
-    width: 200px;
-    padding: 5.5px;
+.error-page-area:before{
+    vertical-align: middle;
     display: inline-block;
-    color: #fff;
-    outline: none;
-    border: none;
-    margin: 0px;
+}
+.error-page-wrap{
+    max-width:580px;
+    padding:20px 0;
+    width:60%;
+    position:relative;
+	margin: 0 auto;
+    background:#fff;
+    box-shadow:0 1px 1px rgba(0,0,0,.1);
+    border-radius:2px;
+    vertical-align: middle;
+    display: inline-block;
 }
 
-/* Change the color of the button on hover */
-.btn-primary:hover {
-    background: #00bbff;
-    border-color: #0062cc;
+.error-page-wrap h2{
+	font-size: 27px;
+	color: #333;
+    margin-top: -15px;
+    margin-bottom: -15px;
+}
+.error-page-wrap p{
+	font-size:17px;
+	color:#333;
+	line-height:24px;
+    padding: 10px;
+}
+.error-page-wrap .btn{
+	background:#ff0080;
+	color:#fff;
+	border-radius:2px;
+	box-shadow: 0 2px 5px rgba(0,0,0,.16), 0 2px 10px rgba(0,0,0,.12);
+	outline:none;
+	margin:0px 5px;
+	font-size:14px;
+    padding: 7px;
+    text-decoration: none;
+}
+.error-page-wrap .btn:hover{
+	background:#00bbff;
+	color:#fff;
+    padding: 7px;
+}
+.error-page-wrap .counter{
+	color:#00c292;
+}
+.error-page-wrap i{
+	font-size: 40px;
+    color: #00c292;
+    padding-bottom: 15px !important;
+    display: block;
 }
 
-        </style>
 
-<?php include_once("footer.php");?>
+
+
+
+
+
+
+
+/* start */
+
+</style>
+
 
 </body>
 </html>	
