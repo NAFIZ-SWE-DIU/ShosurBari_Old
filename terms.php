92aee3a122adada4e029391f1eaa8f493fa89ebe
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-05-14 18:10:37 +0600

    Update terms.php

--- terms.php
+++ terms.php
@@ -3,13 +3,12 @@
 
 <!DOCTYPE HTML>
 <html>
+
+
 <head>
-<title>Find Your Perfect Partner - Makemylove
- | Terms :: Make My Love
-</title>
+<title>Terms & Condition - ShosurBari</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -37,54 +36,136 @@ $(document).ready(function(){
 });
 </script>
 </head>
+
+
 <body>
-<!-- ============================  Navigation Start =========================== -->
-<?php include_once("includes/navigation.php");?>
-<!-- ============================  Navigation End ============================ -->
-<div class="grid_3">
-  <div class="container">
-   <div class="breadcrumb1">
-     <ul>
-        <a href="index.php"><i class="fa fa-home home_1"></i></a>
-        <span class="divider">&nbsp;<|>&nbsp;</span>
-        <li class="current-page"><h4>Terms and Conditions</h4></li>
-     </ul>
-   </div>
-   <div class="terms_1">
-	    <h3>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain </h3>	
-	    <ul class="feature_list">
-			<li><a href="#">1. There are many variations of passages of Lorem Ipsum available.</a></li>
-			<li><a href="#">2. It is a long established fact that a reader.</a></li>
-			<li><a href="#">3. Nulla rutrum nisi mi, iaculis commodo nibh lobortis sed.</a></li>
-			<li><a href="#">4. Sed pulvinar, nunc vitae molestie dapibus, lacus dolor dignissim sapien.</a></li>
-			<li><a href="#">5. Pellentesque ipsum ex, imperdiet quis consequat sed, consectetur ut ante.</a></li>
-			<li><a href="#">6. Aliquam libero felis, mollis vitae elementum vel, bibendum eu tortor.</a></li>
-			<li><a href="#">7. Lorem ipsum dolor sit amet.</a></li>
-		</ul>
-	    <h5>On the other hand, we denounce with righteous indignation and dislike </h5>   
-	    <ul class="feature_list feature_list1">
-			<li><a href="#">There are many variations of passages of Lorem Ipsum available.</a></li>
-			<li><a href="#">It is a long established fact that a reader.</a></li>
-			<li><a href="#">Nulla rutrum nisi mi, iaculis commodo nibh lobortis sed.</a></li>
-			<li><a href="#">Sed pulvinar, nunc vitae molestie dapibus, lacus dolor dignissim sapien.</a></li>
-			<li><a href="#">Pellentesque ipsum ex, imperdiet quis consequat sed, consectetur ut ante.</a></li>
-			<li><a href="#">Aliquam libero felis, mollis vitae elementum vel, bibendum eu tortor.</a></li>
-			<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
-		</ul>
-		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-		<p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-		<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth,</p>
-		<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.</p>
-		<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-		<p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-		<p>But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth,</p>
-		<p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain.</p>
-     </div>
-   </div>
-</div>
+	<!-- ============================  Navigation Start =========================== -->
+	<?php include_once("includes/navigation.php");?>
+	<!-- ============================  Navigation End ============================ -->
+	
+	<div class="grid_3">
+		<div class="container">
+			<div class="breadcrumb1">
+				<ul>
+					<a href="index.php"><i class="fa fa-home home_1"></i></a>
+					<span class="divider">&nbsp;<|>&nbsp;</span>
+					<li class="current-page"><h4>Terms & Condition</h4></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 
 
-<?php include_once("footer.php");?>
+
+	<div class="shosurbari-trmcnd-prvplcy">
+		<div class="shosurbari-security">
+		
+			<h1>ShosurBari Terms & Conditions</h1>
+			<p>These Terms of Use govern your use of the ShosurBari.com website. By accessing or using our website, you agree to be bound by these terms and conditions. Please read them carefully.</p>
+
+			<h3>1. Acceptance of Terms of Use Agreement</h3>
+			<p>By accessing and using ShosurBari.com, you acknowledge that you have read, understood, and agree to be bound by the terms and conditions outlined in this agreement.</p>
+
+			<h3>2. Eligibility</h3>
+			<p>You must be at least 18 years old to create an account and use the services provided by ShosurBari.com. By using our website, you represent and warrant that you have the right, authority, and capacity to enter into this agreement.</p>
+
+			<h3>3. Account Security</h3>
+			<p>It is your responsibility to maintain the confidentiality of your account login information and password. You are solely responsible for any activity that occurs under your account. We implement security measures to protect your personal information, but we cannot guarantee the absolute security of your account.</p>
+
+			<h3>4. Personal Information and Privacy</h3>
+			<p>We collect and process personal information in accordance with our Privacy Policy. By using ShosurBari.com, you consent to the collection, use, and storage of your personal information as described in the Privacy Policy.</p>
+
+			<h3>5. User-Generated Content</h3>
+			<p>When you post or upload content on ShosurBari.com, you retain ownership of the content. However, by posting or uploading content, you grant ShosurBari.com a non-exclusive, royalty-free, worldwide license to use, display, and distribute the content for the purpose of providing our services. You are solely responsible for the content you post and any consequences that may arise from it.</p>
+
+			<h3>6. Prohibited Activities</h3>
+			<p>When using ShosurBari.com, you agree not to engage in any of the following activities:
+			<ol>
+				<li>Violating any applicable laws or regulations.</li>
+				<li>Posting false, misleading, or fraudulent information.</li>
+				<li>Harassing, threatening, or intimidating other users.</li>
+				<li>Uploading or transmitting viruses, malware, or any other malicious code.</li>
+				<li>Interfering with the proper functioning of the website or disrupting the user experience.</li>
+				<li>We reserve the right to suspend or terminate your account if you engage in prohibited activities.</li>
+			</ol>
+			</p>
+
+			<h3>7. Intellectual Property</h3>
+			<p>ShosurBari.com and its content, including logos, trademarks, and other intellectual property, are owned by or licensed to us. You are prohibited from using, reproducing, or distributing any intellectual property without our prior written consent.</p>
+
+			<h3>8. Third-Party Links</h3>
+			<p>ShosurBari.com may contain links to third-party websites or services. We are not responsible for the content, privacy policies, or practices of third-party websites. Your use of third-party websites is at your own risk.</p>
+
+			<h3>9. Limitation of Liability</h3>
+			<p>ShosurBari.com and its owners, employees, or affiliates shall not be liable for any direct, indirect, incidental, consequential, or punitive damages arising out of your use of the website or the services provided. This includes, but is not limited to, any errors, omissions,
+			or delays in the operation of the website, loss of data, or any other loss or damage incurred as a result of your use or reliance on the website or its content.</p>
+
+			<h3>10. Indemnification</h3>
+			<p>You agree to indemnify and hold ShosurBari.com, its owners, employees, and affiliates harmless from any claims, demands, or damages, including attorney's fees, arising out of your use of the website, violation of these terms, or infringement of any rights of a third party.</p>
+
+			<h3>11. Modifications to the Service</h3>
+			<p>We reserve the right to modify, suspend, or discontinue any part of the ShosurBari.com website or its services at any time without prior notice. We are not liable to you or any third party for any modifications, suspensions, or discontinuations of the website or its services.</p>
+
+			<h3>12. Governing Law</h3>
+			<p>These Terms of Use shall be governed by and construed in accordance with the laws of [your jurisdiction]. Any disputes arising under or in connection with these terms shall be subject to the exclusive jurisdiction of the courts of [your jurisdiction].</p>
+
+			<h3>13. Entire Agreement</h3>
+			<p>These Terms of Use constitute the entire agreement between you and ShosurBari.com regarding the use of the website and supersede any prior agreements or understandings.</p>
+
+			<h3>14. Severability</h3>
+			<p>If any provision of these Terms of Use is found to be invalid or unenforceable, the remaining provisions shall remain in full force and effect.</p>
+
+			<h3>15. Waiver</h3>
+			<p>The failure of ShosurBari.com to enforce any right or provision of these Terms of Use shall not be deemed as a waiver of such right or provision.</p>
+
+			<h3>16. Contact Information</h3>
+			<p>If you have any questions or concerns regarding these Terms of Use, please contact us at [contact information].</p>
+
+			<h3>17. Security Act Compliance</h3>
+			<p>ShosurBari.com complies with all applicable security regulations and standards to protect user data and ensure a secure browsing experience.</p>
+
+			<h3>18. Photo</h3>
+			<p>These terms and conditions govern the upload and usage of user photos on ShosurBari.com. By uploading a photo on our website, you agree to comply with these terms and conditions. Please read them carefully.</p>
+			<ul>
+
+				<h4>(A) Ownership of Photos</h4>
+				<p>By uploading a photo on ShosurBari.com, you affirm that you are the owner of the photo or have obtained the necessary permissions to upload and use the photo on our website. You retain ownership of the photo, but you grant ShosurBari.com a non-exclusive, royalty-free, worldwide license to use, display, and distribute the photo for the purpose of providing our services.</p>
+			
+				<h3>(B) Photo Content and Dress Code</h3>
+				<p>When uploading a photo, you agree that it complies with the following guidelines:
+					<ol>
+						<li>The photo must represent you accurately and should not include any misleading or false information.</li>
+						<li>The photo should adhere to the dress code guidelines provided by ShosurBari.com. We do not allow photos with sexual, explicit, or provocative content.</li>
+						<li>You are solely responsible for the content of the photo and any consequences that may arise from its upload or usage.</li>
+						<li>If a user uploads a photo that violates the dress code guidelines or contains explicit content, the web admin has the right to delete the photo.</li>
+					</ol>
+				</p>
+
+				<h3>(C) Photo Deletion and Modification</h3>
+				<p>You have the option to delete or modify your uploaded photo at any time. If you choose to delete your photo, it will be removed from our website, and we will no longer have the right to use, display, or distribute it. However, please note that once a photo is shared with other users or incorporated into other users' accounts, it may be retained by them and we cannot control its further usage.</p>
+				
+				<h3>(D) Security and Confidentiality</h3>
+				<p>ShosurBari.com takes the security and confidentiality of your photos seriously. We implement industry-standard security measures to protect your uploaded photos from unauthorized access, use, or disclosure. However, please be aware that no method of transmission over the internet or electronic storage is 100% secure, and we cannot guarantee absolute security. By uploading a photo, you acknowledge and accept the inherent risks associated with online data transmission.</p>
+				
+				<h3>(E) User Responsibilities</h3>
+				<p>When using ShosurBari.com and accessing user photos, you agree to:
+					<Ol>
+						<li>Respect the privacy and rights of other users.</li>
+						<li>Not download, reproduce, or distribute other users' photos without their explicit permission.</li>
+						<li>Understand that ShosurBari.com does not take responsibility for any misuse or unauthorized sharing of photos by other users.</li>
+					</Ol>
+				</p>
+			</ul>
+
+			<h3>19. Dispute Resolution</h3>
+			<p>Any disputes or claims arising out of or relating to the upload or usage of photos on ShosurBari.com shall be resolved in accordance with the dispute resolution provisions outlined in our Terms and Conditions or as required by applicable laws.</p>
+			<p>If you have any questions or concerns regarding the photo upload terms and conditions, please <a href="contact.php">Contact Us</a>.</p>
+			<p>Please note that these terms and conditions are subject to change without prior notice. It is your responsibility to review and comply with the most up-to-date version of these terms and conditions.</p>
+		</div>
+	</div>
+
+	<?php include_once("footer.php");?>
 
 </body>
 </html>	
+
