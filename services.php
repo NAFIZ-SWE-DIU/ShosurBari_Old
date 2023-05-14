c064c44000b30b89d62a4b68b413bcac5639d38a
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-05-14 22:06:20 +0600

    Update services.php

--- services.php
+++ services.php
@@ -4,15 +4,14 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Find Your Perfect Partner - Makemylove
- | Services :: Make My Love
-</title>
+<title>Services - ShosurBari</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 <link href="css/bootstrap-3.1.1.min.css" rel='stylesheet' type='text/css' />
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/optionsearch.js"></script>
 <script src="js/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <!-- Custom Theme files -->
@@ -37,200 +36,96 @@ $(document).ready(function(){
 });
 </script>
 </head>
+
+
 <body>
-<!-- ============================  Navigation Start =========================== -->
-<?php include_once("includes/navigation.php");?>
-<!-- ============================  Navigation End ============================ -->
-<div class="grid_3">
-  <div class="container">
-   <div class="breadcrumb1">
-     <ul>
-        <a href="index.php"><i class="fa fa-home home_1"></i></a>
-        <span class="divider">&nbsp;<|>&nbsp;</span>
-        <li class="current-page"><h4>Services</h4></li>
-     </ul>
-   </div>
-   <div class="services">
-   	<div class="page_header">
-   	 <h1>Services</h1>
-   	 <h3>There are many variations of passages of Lorem Ipsum available</h3>
-   	 <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-    </div>
-    <div class="services_top">
-    	<div class="col-sm-4 item_content">
-    		<i class="fa fa-heart-o icon_4"> </i>
-    		<h4>Lorem ipsum dolor</h4>
-    		<p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
-    	</div>
-    	<div class="col-sm-4 item_content">
-    		<i class="fa fa-lemon-o icon_4"> </i>
-    		<h4>Lorem ipsum dolor</h4>
-    		<p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
-    	</div>
-    	<div class="col-sm-4 item_content">
-    		<i class="fa fa-thumbs-o-up icon_4"> </i>
-    		<h4>Lorem ipsum dolor</h4>
-    		<p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
-    	</div>
-    	<div class="clearfix"> </div>
-    </div>	
-    <div class="services_top1">
-    	<div class="col-sm-4 item_content">
-    		<i class="fa fa-smile-o icon_4"> </i>
-    		<h4>Lorem ipsum dolor</h4>
-    		<p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
-    	</div>
-    	<div class="col-sm-4 item_content">
-    		<i class="fa fa-wheelchair icon_4"> </i>
-    		<h4>Lorem ipsum dolor</h4>
-    		<p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
-    	</div>
-    	<div class="col-sm-4 item_content">
-    		<i class="fa fa-spinner icon_4"> </i>
-    		<h4>Lorem ipsum dolor</h4>
-    		<p>"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
-    	</div>
-    	<div class="clearfix"> </div>
-    </div>	
-   </div>
-  </div>
-</div>
-<div class="about_middle">
-	<div class="container">
-	  <h2>Happy Clients</h2>
-	  <div class="about_middle-grid1">
-		<div class="col-sm-6 testi_grid list-item-0">
-			<blockquote class="testi_grid_blockquote">
-				<figure class="featured-thumbnail">
-					<img src="images/a1.jpg" class="img-responsive" alt=""/>
-				</figure>
-				<div><a href="#">Aenean nonummy hendrerit mau phasellu porta. Fusce suscipit varius mi sed. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui.…</a>
-				<div class="clearfix"></div>
-				</div>
-			</blockquote>
-		    <small class="testi-meta"><span class="user">Eiusmod tempor incididunt</span></small>
+
+	<!-- ============================  Navigation Start =========================== -->
+	<?php include_once("includes/navigation.php");?>
+	<!-- ============================  Navigation End ============================ -->
+
+
+	<div class="grid_3">
+		<div class="container">
+			<div class="breadcrumb1">
+				<ul>
+					<a href="index.php"><i class="fa fa-home home_1"></i></a>
+					<span class="divider">&nbsp;<|>&nbsp;</span>
+					<li class="current-page"><h4>Services</h4></li>
+				</ul>
+			</div>
 		</div>
-		<div class="col-sm-6 testi_grid list-item-1">
-			<blockquote class="testi_grid_blockquote">
-				<figure class="featured-thumbnail">
-					<img src="images/a2.jpg" class="img-responsive" alt=""/>
-				</figure>
-				<div><a href="#">Aenean nonummy hendrerit mau phasellu porta. Fusce suscipit varius mi sed. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui.…</a>
-				<div class="clearfix"></div>
-				</div>
-			</blockquote>
-			<small class="testi-meta1"><span class="user">Sint occaecat </span></small>
-		</div>
-		<div class="clearfix"> </div>
-	  </div>
-	  <div class="about_middle-grid2">
-		<div class="col-sm-6 testi_grid list-item-0">
-			<blockquote class="testi_grid_blockquote">
-				<figure class="featured-thumbnail">
-					<img src="images/a1.jpg" class="img-responsive" alt=""/>
-				</figure>
-				<div><a href="#">Aenean nonummy hendrerit mau phasellu porta. Fusce suscipit varius mi sed. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui.…</a>
-				<div class="clearfix"></div>
-				</div>
-			</blockquote>
-		    <small class="testi-meta"><span class="user">Eiusmod tempor incididunt</span></small>
-		</div>
-		<div class="col-sm-6 testi_grid list-item-1">
-			<blockquote class="testi_grid_blockquote">
-				<figure class="featured-thumbnail">
-					<img src="images/a2.jpg" class="img-responsive" alt=""/>
-				</figure>
-				<div><a href="#">Aenean nonummy hendrerit mau phasellu porta. Fusce suscipit varius mi sed. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui.…</a>
-				<div class="clearfix"></div>
-				</div>
-			</blockquote>
-			<small class="testi-meta1"><span class="user">Sint occaecat </span></small>
-		</div>
-		<div class="clearfix"> </div>
-	  </div>
 	</div>
-</div>
-<div class="about_bottom">
-	<div class="container">
-		<h3>Team</h3>
-	   <div class="col-md-3 about_grid1">
-		  <ul class="posts-grid our-team">
-			<li class="list-item-1">
-				<figure class="thumbnail_1 thumbnail">
-					<a href="#"><img src="images/a8.jpg"  class="img-responsive" alt=""/></a>
-					<div class="post_networks">
-						<ul>
-							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-						</ul>
-					</div>
-			    </figure>
-			    <div class="desc">
-			    	<h4><a href="#">Perspiciatis</a></h4>
-			    	<p>Lorem ipsum dolor sit amet,</p>
-			    </div>
-			 </li>
-	       </ul>
-	   </div>
-	   <div class="col-md-3 about_grid1">
-		  <ul class="posts-grid our-team">
-			<li class="list-item-1">
-				<figure class="thumbnail_1 thumbnail">
-					<a href="#"><img src="images/a7.jpg"  class="img-responsive" alt=""/></a>
-					<div class="post_networks">
-						<ul>
-							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-						</ul>
-					</div>
-			    </figure>
-			    <div class="desc">
-			    	<h4><a href="#">Aspernatur </a></h4>
-			    	<p>Lorem ipsum dolor sit amet,</p>
-			    </div>
-			 </li>
-	       </ul>
-	   </div>
-	   <div class="col-md-3 about_grid1">
-		  <ul class="posts-grid our-team">
-			<li class="list-item-1">
-				<figure class="thumbnail_1 thumbnail">
-					<a href="#"><img src="images/a6.jpg"  class="img-responsive" alt=""/></a>
-					<div class="post_networks">
-						<ul>
-							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-						</ul>
-					</div>
-			    </figure>
-			    <div class="desc">
-			    	<h4><a href="#">Temporibus</a></h4>
-			    	<p>Lorem ipsum dolor sit amet,</p>
-			    </div>
-			 </li>
-	       </ul>
-	   </div>
-	   <div class="col-md-3 about_grid1">
-		  <ul class="posts-grid our-team">
-			<li class="list-item-1">
-				<figure class="thumbnail_1 thumbnail">
-					<a href="#"><img src="images/a5.jpg"  class="img-responsive" alt=""/></a>
-					<div class="post_networks">
-						<ul>
-							<li class="network_0"><a href="#" title=""><i class="fa fa-facebook"></i></a></li>
-						</ul>
-					</div>
-			    </figure>
-			    <div class="desc">
-			    	<h4><a href="#">Serferendis</a></h4>
-			    	<p>Lorem ipsum dolor sit amet,</p>
-			    </div>
-			 </li>
-	       </ul>
-	   </div>
-	   <div class="clearfix"> </div>
-	</div>
-</div>
 
 
-<?php include_once("footer.php");?>
 
+   <div class="shosurbari-trmcnd-prvplcy">
+   		<div class="page_header">
+			<h1>Our Services</h1>
+			<p>Welcome to ShosurBari.com, your trusted online matrimony service provider. We offer a wide range of services to help you find your perfect life partner. Our platform caters to individuals of all religions, professions, and Bengali communities worldwide. Here is an overview of the services we provide:</p>
+    	</div>
+
+    	<div class="services_top">
+			<div class="col-sm-4 item_content">
+				<i class="fa fa-user-plus icon_4"> </i>
+				<h4>1. Account Creation</h4>
+				<p>Creating an account on ShosurBari.com is easy and free. When you create a new account, you will be asked to provide all the necessary information, including your phone number and email address. However, please note that your phone number and email are always set to "hide" or "only me" mode, ensuring that other users cannot see them. We prioritize the privacy and confidentiality of our users' contact information.</p>
+			</div>
+
+			<div class="col-sm-4 item_content">
+				<i class="fa fa-address-card-o icon_4"> </i>
+				<h4>2. Biodata Posting</h4>
+				<p>Once you have created an account, you can post your biodata, which includes personal information, address, and family details. This information is essential for finding suitable matches based on compatibility and shared values. Your biodata serves as a comprehensive profile that other users can view to assess your suitability as a potential partner.</p>
+			</div>
+
+			<div class="col-sm-4 item_content">
+				<i class="fa fa-camera icon_4"> </i>
+				<h4>3. Profile Photo Upload</h4>
+				<p>You have the option to upload a profile photo to enhance your visibility and attract more potential matches. Your profile photo can be seen by all members of our website, allowing others to get a better sense of your appearance. You have the freedom to delete or modify your profile photo at any time, giving you full control over your visual representation on our platform.</p>
+			</div>
+
+    		<div class="clearfix"> </div>
+    	</div>
+		
+		<div class="services_top1">
+			<div class="col-sm-4 item_content">
+				<i class="fa fa-shield icon_4"> </i>
+				<h4>4. Account Security</h4>
+				<p>We prioritize the security of your account and personal information. You can change your password at any time to ensure the confidentiality of your account. Our platform implements robust security measures to protect your data and provide you with a safe and secure online experience.</p>
+			</div>
+
+			<div class="col-sm-4 item_content">
+				<i class="fa fa-phone icon_4"> </i>
+				<h4>5. Direct Contact</h4>
+				<p>Our platform enables direct contact between users who are interested in each other's biodata. If you find a potential match, you can express your interest by filling out a form and providing your details, along with the required payment. To access the contact details of other users, there will be a nominal fee. You can conveniently make the payment using Personal Bkash, Rocket, or Rocket Mobile Banking.</p>
+			</div>
+
+			<div class="col-sm-4 item_content">
+				<i class="fa fa-money icon_4"> </i>
+				<i class="fa-solid fa-bangladeshi-taka-sign"></i>
+				<!-- <img src="images/bdt-icon.png" alt="BDT Icon"> -->
+				<h4>6. Money Transaction Policy</h4>
+				<p>If you decide to request contact details of another user, you will need to fill out a form and provide your details, along with the necessary transaction details. Please note that there will be charges associated with obtaining the contact details. In the event that a profile you requested turns out to be already married, please inform us within 48 hours. After verifying the correct information, we will refund 90% to 100% of the money you paid</p>
+			</div>
+
+    		<div class="clearfix"> </div>
+    	</div>	
+
+
+		<div class="services_top1">
+			<div class="col-sm-4 item_content">
+				<i class="fa fa-headphones icon_4"> </i>
+				<h4>7. Customer Support</h4>
+				<p>At ShosurBari.com, we are committed to providing excellent customer support throughout your journey. Our dedicated team is available to assist you with any queries, concerns, or technical issues you may encounter. We strive to ensure a seamless and enjoyable experience on our platform.</p>
+				<p>Join ShosurBari.com today and let us assist you in finding your ideal life partner. We are dedicated to helping you make meaningful connections and embark on a journey of love and companionship.</p>
+			</div>
+
+    		<div class="clearfix"> </div>
+    	</div>	
+   	</div>
+
+
+	<?php include_once("footer.php");?>
 </body>
 </html>	
+
