003ab86dc562d6ca6263403366080a12c88a5a91
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-05-18 02:55:13 +0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,6 +5,8 @@ error_reporting(0);
 ?>
 <!DOCTYPE HTML>
 <html>
+
+
 <head>
 <title>Contact Us - ShosurBari</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -43,11 +45,15 @@ $(document).ready(function(){
 });
 </script>
 </head>
+
+
 <body>
-<!-- ============================  Navigation Start =========================== -->
- <?php include_once("includes/navigation.php");?>
-<!-- ============================  Navigation End ============================ -->
-<div class="grid_3">
+  <!-- ============================  Navigation Start =========================== -->
+  <?php include_once("includes/navigation.php");?>
+  <!-- ============================  Navigation End ============================ -->
+
+  
+  <div class="grid_3">
 		<div class="container">
 			<div class="breadcrumb1">
 				<ul>
@@ -64,12 +70,30 @@ $(document).ready(function(){
   <div class="shosurbari-about-contact">
 		<div class="shosurbari-details">
 			<h1>How to Contact ShosurBari? </h1>
-			<p> Vestibulum ante ipsum primis in faucibus orci luctus et trices posuere cubilia 
-				Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue. Nam elit magnandrerit 
-				sit amet tincidunt ac viverra sed nulla. Donec porta diam eu massa. Quisque diam lorem interdum
-				vapibus ac scelerisque vitae pede. Donec eget tellus non erat lacinia fermentum. Donec in velit
-				vel ipsum auctorulvinar. Proin ullamcorper urna et tibulum iaculis lacinia est.
+			<p>
+        We're here to help you! If you have any questions, feedback, or inquiries, please feel free to get
+        in touch with us using the contact form below. Simply provide us with your details and message, and 
+        we'll get back to you as soon as possible.
 			</p>
+
+      <p>
+        Please ensure that you provide accurate contact information so that we can 
+        reach you effectively. We value your privacy and assure you that your personal 
+        details will be handled with utmost confidentiality.
+			</p>
+
+      <p>
+        Our dedicated team is committed to providing excellent customer service, 
+        and we strive to respond to all inquiries promptly. Whether you have a 
+        question about our services, need assistance with your account, or have 
+        any other concerns, we're here to assist you.
+			</p>
+
+      <p>
+        Thank you for choosing shosurbari.com as your trusted matrimonial service 
+        provider. We look forward to serving you and helping you find your perfect match.
+			</p>
+
 		</div>
 	</div>
 
@@ -80,7 +104,7 @@ $(document).ready(function(){
       <div class="flex-container">
         <div class="sb-register-login">
           <div class="sb-biodata-field" style="background: #06b6d4 !important;">
-            <h2>Contact <span>Us</span></h2>
+            <h2>Contact Us</h2>
           </div>
 
           <div class="form-group">
@@ -97,17 +121,6 @@ $(document).ready(function(){
             <input type="tel" id="number_contactus" placeholder="Your Phone Number" name="number_contactus" value="" size="60" minlength="10" maxlength="15" class="form-text required">
             <span id="phone-error" style="font-size: 16px; margin-top: 0px; background: #ffddee; border-radius: 1px 2px 4px 4px; text-align: center; display: none;"></span>
           </div>
-
-          <script>
-            $(document).ready(function() {
-              var input = document.querySelector("#number_contactus");
-              window.intlTelInput(input, {
-                separateDialCode: true,
-                preferredCountries: ["bd"]
-              });
-            });
-          </script>
-
 
           <div class="form-group">
             <textarea rows="4" id="message_contactus" name="message_contactus" placeholder="Type Your Message..." class="form-text-describe required" maxlength="1000"></textarea>
@@ -130,259 +143,276 @@ $(document).ready(function(){
     </form>
   </div>
 
+
+  <?php include_once("footer.php");?>
+
+</body>
+</html>	
+
+
+
+
+
+<!-- Phone Number Country Code With Country Flag -->
 <script>
-  function validateForm() {
-  var name = document.getElementById("name_contactus").value.trim();
-  var email = document.getElementById("email_contactus").value.trim();
-  var phone = document.getElementById("number_contactus").value.trim();
-
-  var messageInput = document.getElementById("message_contactus");
-  var message = messageInput.value.trim();
-
-
-  var nameError = document.getElementById("name-error");
-  var emailError = document.getElementById("email-error");
-  var phoneError = document.getElementById("phone-error");
-  var messageError = document.getElementById("message-error");
-  var valid = true;
-
-  // Validate name
-
-
-
-//Full Name validation
-        if (name == "") {
-        document.getElementById('name_contactus').style.borderColor = "red";
-        document.getElementById('name_contactus').scrollIntoView({
-          behavior: 'smooth',
-          block: 'center',
-        });
-
-        var errorDiv = document.getElementById('name-error');
-        errorDiv.innerHTML = "Please enter your Full Name !";
-        errorDiv.style.display = 'block';
-        errorDiv.classList.add('fade-in');
-
-        // Change color multiple times
-        var colors = ['green', 'blue', 'red'];
-        var colorIndex = 0;
-        setInterval(function() {
-          errorDiv.style.color = colors[colorIndex];
-          colorIndex = (colorIndex + 1) % colors.length;
-        }, 500);
-
-        return false;
-      }else{
-        document.getElementById('name_contactus').style.borderColor = "green";
-        document.getElementById('name-error').innerHTML = "";
-      }
-
-
-            //Email validation
-            if (email == "") {
-        document.getElementById('email_contactus').style.borderColor = "red";
-        document.getElementById('email_contactus').scrollIntoView({
-          behavior: 'smooth',
-          block: 'center',
-        });
-
-        var errorDiv = document.getElementById('email-error');
-        errorDiv.innerHTML = "Please enter your Email !";
-        errorDiv.style.display = 'block';
-        errorDiv.classList.add('fade-in');
-
-        // Change color multiple times
-        var colors = ['green', 'blue', 'red'];
-        var colorIndex = 0;
-        setInterval(function() {
-          errorDiv.style.color = colors[colorIndex];
-          colorIndex = (colorIndex + 1) % colors.length;
-        }, 500);
-
-        return false;
-      }else if(! /^[a-zA-Z0-9._-]+@(gmail|outlook|hotmail|yahoo).com$/.test(email)){
-        document.getElementById('email_contactus').style.borderColor = "red";
-        document.getElementById('email_contactus').scrollIntoView({
-          behavior: 'smooth',
-          block: 'center',
-        });
-
-        var errorDiv = document.getElementById('email-error');
-        errorDiv.innerHTML = "Please Enter a Valid Email. Only Used: (@gmail or @outlook or @hotmail or @yahoo).com";
-        errorDiv.style.display = 'block';
-        errorDiv.classList.add('fade-in');
-
-        // Change color multiple times
-        var colors = ['green', 'blue', 'red'];
-        var colorIndex = 0;
-        setInterval(function() {
-          errorDiv.style.color = colors[colorIndex];
-          colorIndex = (colorIndex + 1) % colors.length;
-        }, 500);
-
-        return false;
-      }else{
-        document.getElementById('email_contactus').style.borderColor = "green";
-        document.getElementById('email-error').innerHTML = "";
-      }
-
-
-
-
-  //Phone number validation
-var phoneInput = document.getElementById("number_contactus");
-var phone = phoneInput.value.replace(/[^\d]/g, ''); // Remove any non-digit characters from the input
-
-if (phone.length === 0) {
-  phoneInput.style.borderColor = "red";
-  phoneInput.scrollIntoView({
-    behavior: 'smooth',
-    block: 'center',
+  $(document).ready(function() {
+    var input = document.querySelector("#number_contactus");
+    window.intlTelInput(input, {
+    separateDialCode: true,
+    preferredCountries: ["bd"]
+    });
   });
-
-  var errorDiv = document.getElementById('phone-error');
-  errorDiv.innerHTML = "Please enter your phone number!";
-  errorDiv.style.display = 'block';
-  errorDiv.classList.add('fade-in');
-
-  // Change color multiple times
-  var colors = ['green', 'blue', 'red'];
-  var colorIndex = 0;
-  setInterval(function() {
-    errorDiv.style.color = colors[colorIndex];
-    colorIndex = (colorIndex + 1) % colors.length;
-  }, 500);
-
-  return false;
-} else if (phone.length < 10 || phone.length > 15) {
-  phoneInput.style.borderColor = "red";
-  phoneInput.scrollIntoView({
-    behavior: 'smooth',
-    block: 'center',
-  });
-
-  var errorDiv = document.getElementById('phone-error');
-  errorDiv.innerHTML = "Phone number must be between 10 and 15 digits, Remove any non-digit characters & space from the input.";
-  errorDiv.style.display = 'block';
-  errorDiv.classList.add('fade-in');
-
-  // Change color multiple times
-  var colors = ['green', 'blue', 'red'];
-  var colorIndex = 0;
-  setInterval(function() {
-    errorDiv.style.color = colors[colorIndex];
-    colorIndex = (colorIndex + 1) % colors.length;
-  }, 500);
-
-  return false;
-} else {
-  phoneInput.style.borderColor = "green";
-  document.getElementById('phone-error').innerHTML = "";
-}
-
-
-
-
-
-
-// Validate Message
-if (message === "") {
-  messageInput.style.borderColor = "red";
-  messageInput.scrollIntoView({
-    behavior: 'smooth',
-    block: 'center',
-  });
-
-
-
-  var errorDiv = document.getElementById('message-error');
-        errorDiv.innerHTML = "Please type your message !";
-        errorDiv.style.display = 'block';
-        errorDiv.classList.add('fade-in');
-
-        // Change color multiple times
-        var colors = ['green', 'blue', 'red'];
-        var colorIndex = 0;
-        setInterval(function() {
-          errorDiv.style.color = colors[colorIndex];
-          colorIndex = (colorIndex + 1) % colors.length;
-        }, 500);
-
-  return false;
-} else {
-  messageInput.style.borderColor = "green";
-  document.getElementById('message-error').innerHTML = "";
-}
-
-
-  return valid;
-}
 </script>
 
 
 
 
 
+<!-- Contact Form Validation -->
+<script>
+  function validateForm() {
+    var name = document.getElementById("name_contactus").value.trim();
+    var email = document.getElementById("email_contactus").value.trim();
+    var phone = document.getElementById("number_contactus").value.trim();
+
+    var messageInput = document.getElementById("message_contactus");
+    var message = messageInput.value.trim();
+
+    var nameError = document.getElementById("name-error");
+    var emailError = document.getElementById("email-error");
+    var phoneError = document.getElementById("phone-error");
+    var messageError = document.getElementById("message-error");
+    var valid = true;
+
+
+
+
+    //Full Name validation
+    if (name == "") {
+      document.getElementById('name_contactus').style.borderColor = "red";
+      document.getElementById('name_contactus').scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      });
+
+      var errorDiv = document.getElementById('name-error');
+      errorDiv.innerHTML = "Please enter your Full Name !";
+      errorDiv.style.display = 'block';
+      errorDiv.classList.add('fade-in');
+
+      // Change color multiple times
+      var colors = ['green', 'blue', 'red'];
+      var colorIndex = 0;
+      setInterval(function() {
+      errorDiv.style.color = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length;
+      }, 500);
+
+    return false;
+    }else{
+    document.getElementById('name_contactus').style.borderColor = "green";
+    document.getElementById('name-error').innerHTML = "";
+    }
 
 
 
 
 
+    //Email validation
+    if (email == "") {
+      document.getElementById('email_contactus').style.borderColor = "red";
+      document.getElementById('email_contactus').scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      });
+
+      var errorDiv = document.getElementById('email-error');
+      errorDiv.innerHTML = "Please enter your Email !";
+      errorDiv.style.display = 'block';
+      errorDiv.classList.add('fade-in');
+
+      // Change color multiple times
+      var colors = ['green', 'blue', 'red'];
+      var colorIndex = 0;
+      setInterval(function() {
+      errorDiv.style.color = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length;
+      }, 500);
+
+    return false;
+
+    }else if(! /^[a-zA-Z0-9._-]+@(gmail|outlook|hotmail|yahoo).com$/.test(email)){
+      document.getElementById('email_contactus').style.borderColor = "red";
+      document.getElementById('email_contactus').scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      });
+
+      var errorDiv = document.getElementById('email-error');
+      errorDiv.innerHTML = "Please Enter a Valid Email. Only Used: (@gmail or @outlook or @hotmail or @yahoo).com";
+      errorDiv.style.display = 'block';
+      errorDiv.classList.add('fade-in');
+
+      // Change color multiple times
+      var colors = ['green', 'blue', 'red'];
+      var colorIndex = 0;
+      setInterval(function() {
+      errorDiv.style.color = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length;
+      }, 500);
+
+    return false;
+
+    }else{
+    document.getElementById('email_contactus').style.borderColor = "green";
+    document.getElementById('email-error').innerHTML = "";
+    }
+
+
+
+
+    //Phone number validation
+    var phoneInput = document.getElementById("number_contactus");
+    var phone = phoneInput.value.replace(/[^\d]/g, ''); // Remove any non-digit characters from the input
+
+    if (phone.length === 0) {
+      phoneInput.style.borderColor = "red";
+      phoneInput.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      });
+
+      var errorDiv = document.getElementById('phone-error');
+      errorDiv.innerHTML = "Please enter your phone number!";
+      errorDiv.style.display = 'block';
+      errorDiv.classList.add('fade-in');
+
+      // Change color multiple times
+      var colors = ['green', 'blue', 'red'];
+      var colorIndex = 0;
+      setInterval(function() {
+      errorDiv.style.color = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length;
+      }, 500);
+
+    return false;
+
+    } else if (phone.length < 10 || phone.length > 15) {
+      phoneInput.style.borderColor = "red";
+      phoneInput.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      });
+
+      var errorDiv = document.getElementById('phone-error');
+      errorDiv.innerHTML = "Phone number must be between 10 and 15 digits, Remove any non-digit characters & space from the input.";
+      errorDiv.style.display = 'block';
+      errorDiv.classList.add('fade-in');
+
+      // Change color multiple times
+      var colors = ['green', 'blue', 'red'];
+      var colorIndex = 0;
+      setInterval(function() {
+      errorDiv.style.color = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length;
+      }, 500);
+
+    return false;
+
+    } else {
+    phoneInput.style.borderColor = "green";
+    document.getElementById('phone-error').innerHTML = "";
+    }
+
+
+
+
+    // Validate Message
+    if (message === "") {
+      messageInput.style.borderColor = "red";
+      messageInput.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+      });
+
+
+
+      var errorDiv = document.getElementById('message-error');
+      errorDiv.innerHTML = "Please type your message !";
+      errorDiv.style.display = 'block';
+      errorDiv.classList.add('fade-in');
+
+      // Change color multiple times
+      var colors = ['green', 'blue', 'red'];
+      var colorIndex = 0;
+      setInterval(function() {
+      errorDiv.style.color = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length;
+      }, 500);
+
+    return false;
+
+    } else {
+    messageInput.style.borderColor = "green";
+    document.getElementById('message-error').innerHTML = "";
+    }
+
+
+  return valid;
+  }
+</script>
+
+
+
+
+
+<!-- After Submit the FeedBack then Show PoP Up Message -->
 <script> 
-function showSuccessMessage() {
-  // Show the overlay
-  document.querySelector('.overlay').style.display = 'block';
+  function showSuccessMessage() {
+    // Show the overlay
+    document.querySelector('.overlay').style.display = 'block';
 
-  // Show the popup message
-  var popup = document.querySelector('.popup-message');
-  popup.style.display = 'block';
+    // Show the popup message
+    var popup = document.querySelector('.popup-message');
+    popup.style.display = 'block';
 
-  // Set the message text
-  popup.querySelector('h3').innerHTML = 'Thank you!';
-  popup.querySelector('p').innerHTML = 'Your data has been saved to the database.';
+    // Set the message text
+    popup.querySelector('h3').innerHTML = 'Thank you!';
+    popup.querySelector('p').innerHTML = 'Your data has been saved to the database.';
 
-  // Add a close button to the popup message
-  var closeButton = document.createElement('button');
-  closeButton.innerHTML = 'Close';
-  closeButton.classList.add('close-button');
-  popup.appendChild(closeButton);
+    // Add a close button to the popup message
+    var closeButton = document.createElement('button');
+    closeButton.innerHTML = 'Close';
+    closeButton.classList.add('close-button');
+    popup.appendChild(closeButton);
 
-  // Hide the popup and overlay when the close button is clicked
-  closeButton.addEventListener('click', function() {
+    // Hide the popup and overlay when the close button is clicked
+    closeButton.addEventListener('click', function() {
     popup.style.display = 'none';
     document.querySelector('.overlay').style.display = 'none';
-  });
-}
+    });
+  }
 
-// Change the form submission code to the following
-$('form[name="myForm"]').submit(function(e) {
-  e.preventDefault(); // Prevent the default form submission
+  // Change the form submission code
+  $('form[name="myForm"]').submit(function(e) {
+    e.preventDefault(); // Prevent the default form submission
 
-  if (validateForm()) {
-    // Submit the form data using AJAX
-    $.ajax({
-      url: 'contact.php', // Replace this with the URL of your server-side script
-      type: 'POST',
-      data: $(this).serialize(),
-      success: function(response) {
+    if (validateForm()) {
+      // Submit the form data using AJAX
+      $.ajax({
+        url: 'contact.php',
+        type: 'POST',
+        data: $(this).serialize(),
+        success: function(response) {
         // Show the success message
         showSuccessMessage();
 
         // Clear the form
         $('form[name="myForm"]')[0].reset();
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        // Handle errors here
-      }
-    });
-  }
-});
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+        }
+      });
+    }
+  });
 </script>
-
-
-
-
-<?php include_once("footer.php");?>
-
-</body>
-</html>	
