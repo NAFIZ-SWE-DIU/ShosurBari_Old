b5de1448142644978e283fa2cef00e8815131c21
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-05-21 04:43:49 +0600

    Update editdemo.php

--- editdemo.php
+++ editdemo.php
@@ -113,15 +113,15 @@ $username=$row['username'];
 <form id="biodataForm">
   <!-- progressbar -->
     <ul id="progressbar">
-        <li class="active">Bio 1</li>
-        <li> Bio 2</li>
-        <li>Bio 3</li>
-        <li>Bio 4</li>
-        <li>Bio 5</li>
-        <li> Bio 6</li>
-        <li>Bio 7</li>
-        <li>Bio 8 </li>
-        <li>Bio 9 </li>
+        <li class="active">Physical</li>
+        <li> LifeStyle</li>
+        <li>Education</li>
+        <li>Address</li>
+        <li>Family</li>
+        <li>MarriageInfo</li>
+        <li> MarriageInfo</li>
+        <li>Religion</li>
+        <li>LifePartner</li>
     </ul>
 
 
@@ -1613,13 +1613,6 @@ $partner_attributes=$row['partner_attributes'];
 }
 
 
-.fs-subtitle {
-    font-weight: normal;
-    font-size: 13px;
-    color: #666;
-    margin-bottom: 20px;
-}
-
 /*progressbar*/
 #progressbar {
     background: #fff;
@@ -1632,9 +1625,8 @@ $partner_attributes=$row['partner_attributes'];
 
 #progressbar li {
     list-style-type: none;
-    color: #06b6d4;
-    text-transform: uppercase;
-    font-size: 14px;
+    color: #000;
+    font-size: 15px;
     width: 11%;
     float: left;
     position: relative;
@@ -1644,23 +1636,25 @@ $partner_attributes=$row['partner_attributes'];
 #progressbar li:before {
     content: counter(step);
     counter-increment: step;
-    width: 20px;
-    line-height: 20px;
+    width: 34px;
+    line-height: 31px;
     display: block;
     font-size: 10px;
     color: #fff;
-    background: #0aa4ca;
-    border-radius: 3px;
-    margin: 0 auto 5px auto;
+    background: #06b6d4;
+    border-radius: 50%;
+    margin: -5px auto 0px auto;
+    font-size: 14px;
 }
 
 #progressbar li:after {
     content: '';
     width: 100%;
-    height: 5px;
+    height: 4px;
+    border-radius: 0px;
     background: #06b6d4;
     position: absolute;
-    left: -42%;
+    left: -35%;
     top: 9px;
     z-index: -1; /* Ensure the connectors appear behind the list items */
 }
@@ -1680,85 +1674,46 @@ $partner_attributes=$row['partner_attributes'];
 
 
 <script>
-//jQuery time
-var current_fs, next_fs, previous_fs; //fieldsets
-var left, opacity, scale; //fieldset properties which we will animate
-var animating; //flag to prevent quick multi-click glitches
+// jQuery time
+var current_fs, next_fs, previous_fs; // fieldsets
 
-$(".next").click(function(){
-  if(animating) return false;
-  animating = true;
-  
+$(".next").click(function() {
   current_fs = $(this).parent();
   next_fs = $(this).parent().next();
-  
-  //activate next step on progressbar using the index of next_fs
+
+  // Activate next step on progressbar using the index of next_fs
   $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-  
-  //show the next fieldset
-  next_fs.show(); 
-  //hide the current fieldset with style
-  current_fs.animate({opacity: 0}, {
-    step: function(now, mx) {
-      //as the opacity of current_fs reduces to 0 - stored in "now"
-      //1. scale current_fs down to 80%
-      scale = 1 - (1 - now) * 0.2;
-      //2. bring next_fs from the right(50%)
-      left = (now * 50)+"%";
-      //3. increase opacity of next_fs to 1 as it moves in
-      opacity = 1 - now;
-      current_fs.css({'transform': 'scale('+scale+')'});
-      next_fs.css({'left': left, 'opacity': opacity});
-    }, 
-    duration: 800, 
-    complete: function(){
-      current_fs.hide();
-      animating = false;
-    }, 
-    //this comes from the custom easing plugin
-    easing: 'easeInOutBack'
-  });
+
+  // Show the next fieldset
+  next_fs.show();
+  // Hide the current fieldset
+  current_fs.hide();
+
+  // Smooth scroll to the top of the progress bar
+  $('html, body').animate({ scrollTop: $('#progressbar').offset().top }, 800);
 });
 
-$(".previous").click(function(){
-  if(animating) return false;
-  animating = true;
-  
+$(".previous").click(function() {
   current_fs = $(this).parent();
   previous_fs = $(this).parent().prev();
-  
-  //de-activate current step on progressbar
+
+  // De-activate current step on progressbar
   $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-  
-  //show the previous fieldset
-  previous_fs.show(); 
-  //hide the current fieldset with style
-  current_fs.animate({opacity: 0}, {
-    step: function(now, mx) {
-      //as the opacity of current_fs reduces to 0 - stored in "now"
-      //1. scale previous_fs from 80% to 100%
-      scale = 0.8 + (1 - now) * 0.2;
-      //2. take current_fs to the right(50%) - from 0%
-      left = ((1-now) * 50)+"%";
-      //3. increase opacity of previous_fs to 1 as it moves in
-      opacity = 1 - now;
-      current_fs.css({'left': left});
-      previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-    }, 
-    duration: 800, 
-    complete: function(){
-      current_fs.hide();
-      animating = false;
-    }, 
-    //this comes from the custom easing plugin
-    easing: 'easeInOutBack'
-  });
+
+  // Show the previous fieldset
+  previous_fs.show();
+  // Hide the current fieldset
+  current_fs.hide();
+
+  // Smooth scroll to the top of the progress bar
+  $('html, body').animate({ scrollTop: $('#progressbar').offset().top }, 800);
 });
 
-$(".submit").click(function(){
+$(".submit").click(function() {
   return false;
-})
+});
 </script>
+
 
 
 
