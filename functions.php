bcdbec05d968352e900c1b1710e1a7be413e64bc
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-05-29 23:21:47 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -148,6 +148,8 @@
     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
     function register(){
+        global $conn;
+
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $fname=$_POST['fname'];
             $uname=$_POST['uname'];
@@ -395,17 +397,306 @@
     --                                               --
     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+
+    function post_biodata($id){
+
+    //Biodata 1 
+	$biodatagender=$_POST['biodatagender'];
+	$day=$_POST['day'];
+	$month=$_POST['month'];
+	$year=$_POST['year'];
+	$dob=$day ."-" . $month . "-" .$year ;
+	$height=$_POST['height'];
+	$weight=$_POST['weight'];	
+	$physicalstatus=$_POST['physicalstatus'];
+	$Skin_tones = $_POST['Skin_tones'];
+	$bloodgroup=$_POST['bloodgroup'];
+
+	//Biodata 2
+	$smoke=$_POST['smoke'];
+	$occupation_sector=$_POST['occupation_sector'];
+	$other_occupation_sector=$_POST['other_occupation_sector'];
+	$business_occupation_level=$_POST['business_occupation_level'];
+	$student_occupation_level=$_POST['student_occupation_level'];
+    $health_occupation_level=$_POST['health_occupation_level'];
+	$engineer_occupation_level=$_POST['engineer_occupation_level'];
+	$teacher_occupation_level=$_POST['teacher_occupation_level'];
+	$defense_occupation_level=$_POST['defense_occupation_level'];
+	$foreigner_occupation_level=$_POST['foreigner_occupation_level'];
+	$garments_occupation_level=$_POST['garments_occupation_level'];
+	$driver_occupation_level=$_POST['driver_occupation_level'];
+	$service_andcommon_occupation_level=$_POST['service_andcommon_occupation_level'];
+	$mistri_occupation_level=$_POST['mistri_occupation_level'];
+	$occupation_describe=$_POST['occupation_describe'];
+	$dress_code=$_POST['dress_code'];
+	$aboutme=$_POST['aboutme'];		
+
+	//Biodata 3
+	$secondary_edu_method=$_POST['secondary_edu_method'];
+	$maxedu_qualification=$_POST['maxedu_qualification'];
+	$qawmi_madrasa_hafez=$_POST['qawmi_madrasa_hafez'];
+	$qawmimadrasa_dawrapass=$_POST['qawmimadrasa_dawrapass'];
+	$kowmi_dawrapas_year=$_POST['kowmi_dawrapas_year'];
+	$kowmi_current_edu_level=$_POST['kowmi_current_edu_level'];
+	$gnrl_mdrs_secondary_pass=$_POST['gnrl_mdrs_secondary_pass'];
+	$gnrl_mdrs_secondary_pass_year=$_POST['gnrl_mdrs_secondary_pass_year'];
+	$gnrl_mdrs_secondary_end_year=$_POST['gnrl_mdrs_secondary_end_year'];
+	$gnrlmdrs_secondary_running_std=$_POST['gnrlmdrs_secondary_running_std'];
+	$higher_secondary_edu_method=$_POST['higher_secondary_edu_method'];
+	$gnrlmdrs_hrsecondary_pass=$_POST['gnrlmdrs_hrsecondary_pass'];
+	$gnrlmdrs_hrsecondary_pass_year=$_POST['gnrlmdrs_hrsecondary_pass_year'];
+	$gnrlmdrs_hrsecondary_exam_year=$_POST['gnrlmdrs_hrsecondary_exam_year'];
+	$gnrlmdrs_hrsecondary_group=$_POST['gnrlmdrs_hrsecondary_group'];
+	$gnrlmdrs_hrsecondary_rningstd=$_POST['gnrlmdrs_hrsecondary_rningstd'];
+	$diploma_hrsecondary_pass=$_POST['diploma_hrsecondary_pass'];
+	$diploma_hrsecondary_pass_year=$_POST['diploma_hrsecondary_pass_year'];
+	$diploma_hrsecondary_sub=$_POST['diploma_hrsecondary_sub'];
+	$diploma_hrsecondary_endingyear=$_POST['diploma_hrsecondary_endingyear'];
+	$varsity_edu_method=$_POST['varsity_edu_method'];
+	$university_pass=$_POST['university_pass'];
+	$varsity_passing_year=$_POST['varsity_passing_year'];
+	$varsity_subject=$_POST['varsity_subject'];
+	$varsity_ending_year=$_POST['varsity_ending_year'];
+	$university_name=$_POST['university_name'];
+	$others_edu_qualification=$_POST['others_edu_qualification'];
+
+	//Biodata 4
+	$permanent_division=$_POST['permanent_division'];
+	$home_district_under_barishal=$_POST['home_district_under_barishal'];
+    $home_district_under_chattogram=$_POST['home_district_under_chattogram'];
+	$home_district_under_dhaka=$_POST['home_district_under_dhaka'];
+	$home_district_under_khulna=$_POST['home_district_under_khulna'];
+	$home_district_under_mymensingh=$_POST['home_district_under_mymensingh'];
+	$home_district_under_rajshahi=$_POST['home_district_under_rajshahi'];
+	$home_district_under_rangpur=$_POST['home_district_under_rangpur'];
+	$home_district_under_sylhet=$_POST['home_district_under_sylhet'];
+
+	$country_present_address=$_POST['country_present_address'];
+	$present_address_location=$_POST['present_address_location'];
+	$childhood=$_POST['childhood'];
+
+	//Biodata 5
+	$father_alive=$_POST['father_alive'];
+	$fatheroccupation=$_POST['fatheroccupation'];
+	$mother_alive=$_POST['mother_alive'];
+	$motheroccupation=$_POST['motheroccupation'];
+	$brosis_number=$_POST['brosis_number'];
+	$brosis_info=$_POST['brosis_info'];
+	$uncle_profession=$_POST['uncle_profession'];
+	$family_class=$_POST['family_class'];
+	$financial_condition=$_POST['financial_condition'];
+	$family_religious_condition=$_POST['family_religious_condition'];
+
+	// Marital Status - Biodata 6 & 7
+	$maritalstatus=$_POST['maritalstatus'];
+	// Divorce
+	$divorce_reason=$_POST['divorce_reason'];
+	$divorce_how_many_son=$_POST['divorce_how_many_son'];
+	$divorce_son_details=$_POST['divorce_son_details'];
+	// Widow
+	$how_widow=$_POST['how_widow'];
+	$widow_how_many_son=$_POST['widow_how_many_son'];
+	$widow_son_details=$_POST['widow_son_details'];
+	// Widower
+	$how_widower=$_POST['how_widower'];
+	$widower_how_many_son=$_POST['widower_how_many_son'];
+	$widower_son_details=$_POST['widower_son_details'];
+	// Married
+	$get_wife_permission=$_POST['get_wife_permission'];
+	$get_family_permission=$_POST['get_family_permission'];
+	$why_again_married=$_POST['why_again_married'];
+	$married_how_many_son=$_POST['married_how_many_son'];
+	$married_son_details=$_POST['married_son_details'];
+
+	//Biodata 6
+	$guardians_agree=$_POST['guardians_agree'];
+	$allowstudy_aftermarriage=$_POST['allowstudy_aftermarriage'];
+	$allowjob_aftermarriage=$_POST['allowjob_aftermarriage'];
+	$livewife_aftermarriage=$_POST['livewife_aftermarriage'];
+	$profileby=$_POST['profileby'];
+ 
+	//Biodata 7
+	$guardians_agree=$_POST['guardians_agree'];
+	$anyjob_aftermarriage=$_POST['anyjob_aftermarriage'];
+	$studies_aftermarriage=$_POST['studies_aftermarriage'];
+	$agree_marriage_student=$_POST['agree_marriage_student'];
+	$profileby=$_POST['profileby'];
+
+	//Biodata 8
+	$religion=$_POST['religion'];
+	$yourreligion_condition=$_POST['yourreligion_condition'];
+
+	//Biodata 9
+	$partner_religius=$_POST['partner_religius'];
+	$partner_district=$_POST['partner_district'];
+	$partner_maritialstatus=$_POST['partner_maritialstatus'];
+	$partner_age=$_POST['partner_age'];
+	$partner_skintones=$_POST['partner_skintones'];
+	$partner_height=$_POST['partner_height'];
+	$partner_education=$_POST['partner_education'];
+	$partner_profession=$_POST['partner_profession'];
+	$partner_financial=$_POST['partner_financial'];
+	$partner_attributes=$_POST['partner_attributes'];
+
     require_once("includes/dbconn.php");
 
-    function insertData($table, $data){
-        global $conn;
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+--      Personal & Physical  / sb-biodata-1      --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+    $sql = "INSERT 
+	INTO
+	1bd_personal_physical
+	(user_id, biodatagender, dateofbirth, height, weight, physicalstatus, Skin_tones, bloodgroup, profilecreationdate  ) 
+	VALUES
+	('$id', '$biodatagender', '$dob', '$height', '$weight', '$physicalstatus', '$Skin_tones', '$bloodgroup', DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+    if (mysqli_query($conn,$sql))
+    { echo " ";}
 
-        $columns = implode(", ", array_keys($data));
-        $values = implode("', '", array_values($data));
 
-        $sql = "INSERT INTO $table ($columns) VALUES ('$values')";
-        mysqli_query($conn, $sql);
-    }
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+--     Personal & Life Style  / sb-biodata-2     --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+    $sql = "INSERT INTO 2bd_personal_lifestyle
+	(user_id, smoke, occupation_sector, other_occupation_sector, business_occupation_level, student_occupation_level, health_occupation_level, engineer_occupation_level, teacher_occupation_level, defense_occupation_level, foreigner_occupation_level, garments_occupation_level, driver_occupation_level, service_andcommon_occupation_level, mistri_occupation_level, occupation_describe, dress_code, aboutme, profilecreationdate  ) 
+	VALUES
+	('$id', '$smoke', '$occupation_sector', '$other_occupation_sector', '$business_occupation_level', '$student_occupation_level', '$health_occupation_level', '$engineer_occupation_level', '$teacher_occupation_level', '$defense_occupation_level', '$foreigner_occupation_level', '$garments_occupation_level', '$driver_occupation_level', '$service_andcommon_occupation_level', '$mistri_occupation_level', '$occupation_describe', '$dress_code', '$aboutme', DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+    if (mysqli_query($conn,$sql))
+	{echo " ";}
+
+
+
+
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+--  Educational Qualifications  / sb-biodata-3   --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --*/
+$sql = "INSERT INTO 3bd_educational_qualifications
+(user_id, secondary_edu_method, maxedu_qualification, qawmi_madrasa_hafez, qawmimadrasa_dawrapass, kowmi_dawrapas_year, kowmi_current_edu_level, gnrl_mdrs_secondary_pass, gnrl_mdrs_secondary_pass_year, gnrl_mdrs_secondary_end_year, gnrlmdrs_secondary_running_std, higher_secondary_edu_method, gnrlmdrs_hrsecondary_pass, gnrlmdrs_hrsecondary_pass_year, gnrlmdrs_hrsecondary_exam_year, gnrlmdrs_hrsecondary_group, gnrlmdrs_hrsecondary_rningstd, diploma_hrsecondary_pass, diploma_hrsecondary_pass_year, diploma_hrsecondary_sub, diploma_hrsecondary_endingyear, varsity_edu_method, university_pass, varsity_passing_year, varsity_subject, varsity_ending_year, university_name, others_edu_qualification, profilecreationdate  ) 
+VALUES
+('$id', '$secondary_edu_method', '$maxedu_qualification', '$qawmi_madrasa_hafez', '$qawmimadrasa_dawrapass', '$kowmi_dawrapas_year', '$kowmi_current_edu_level', '$gnrl_mdrs_secondary_pass', '$gnrl_mdrs_secondary_pass_year', '$gnrl_mdrs_secondary_end_year', '$gnrlmdrs_secondary_running_std', '$higher_secondary_edu_method', '$gnrlmdrs_hrsecondary_pass', '$gnrlmdrs_hrsecondary_pass_year', '$gnrlmdrs_hrsecondary_exam_year', '$gnrlmdrs_hrsecondary_group', '$gnrlmdrs_hrsecondary_rningstd', '$diploma_hrsecondary_pass', '$diploma_hrsecondary_pass_year', '$diploma_hrsecondary_sub', '$diploma_hrsecondary_endingyear', '$varsity_edu_method', '$university_pass', '$varsity_passing_year', '$varsity_subject', '$varsity_ending_year', '$university_name', '$others_edu_qualification', DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+if (mysqli_query($conn,$sql))
+{echo " ";}
+
+
+
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+-- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
+--                S  T  A  R  T                  --
+--       Address Details  /  sb-biodata-4        --
+--                                               --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+    $sql = "INSERT INTO 4bd_address_details
+    (user_id, permanent_division, home_district_under_barishal, home_district_under_chattogram, home_district_under_dhaka, home_district_under_khulna, home_district_under_mymensingh, home_district_under_rajshahi, home_district_under_rangpur, home_district_under_sylhet, country_present_address, present_address_location, childhood, profilecreationdate  ) 
+    VALUES
+	('$id', '$permanent_division', '$home_district_under_barishal', '$home_district_under_chattogram', '$home_district_under_dhaka', '$home_district_under_khulna', '$home_district_under_mymensingh', '$home_district_under_rajshahi', '$home_district_under_rangpur', '$home_district_under_sylhet', '$country_present_address', '$present_address_location', '$childhood', DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+    if (mysqli_query($conn,$sql))
+	{echo " ";}
+
+
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+-- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
+--                S  T  A  R  T                  --
+--     Family Information  / sb-biodata-5        --
+--                                               --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+$sql = "INSERT INTO 5bd_family_information
+(user_id, father_alive, fatheroccupation, mother_alive, motheroccupation, brosis_number, brosis_info, uncle_profession, family_class, financial_condition, family_religious_condition, profilecreationdate  ) 
+VALUES
+('$id', '$father_alive', '$fatheroccupation', '$mother_alive', '$motheroccupation', '$brosis_number', '$brosis_info', '$uncle_profession', '$family_class', '$financial_condition', '$family_religious_condition', DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+if (mysqli_query($conn,$sql))
+{echo " ";}
+
+
+
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+-- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
+--                S  T  A  R  T                  --
+--  Marriage related Info /Marital Status 6 & 7  --
+--                                               --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+$sql = "INSERT INTO 6bd_7bd_marital_status
+(user_id, maritalstatus, divorce_reason, divorce_how_many_son, divorce_son_details, how_widow, widow_how_many_son, widow_son_details, how_widower, widower_how_many_son, widower_son_details, get_wife_permission, get_family_permission, why_again_married, married_how_many_son, married_son_details, profilecreationdate  ) 
+VALUES
+('$id', '$maritalstatus', '$divorce_reason', '$divorce_how_many_son', '$divorce_son_details', '$how_widow', '$widow_how_many_son', '$widow_son_details', '$how_widower', '$widower_how_many_son', '$widower_son_details', '$get_wife_permission', '$get_family_permission', '$why_again_married', '$married_how_many_son', '$married_son_details',  DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+if (mysqli_query($conn,$sql))
+{echo " ";}
+
+
+
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+--   Male Marriage related Info / sb-biodata-6   --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+    $sql = "INSERT INTO 6bd_marriage_related_qs_male
+	(user_id, guardians_agree, allowstudy_aftermarriage, allowjob_aftermarriage, livewife_aftermarriage, profileby, profilecreationdate  ) 
+    VALUES
+	('$id', '$guardians_agree', '$allowstudy_aftermarriage', '$allowjob_aftermarriage', '$livewife_aftermarriage', '$profileby',  DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+    if (mysqli_query($conn,$sql))
+	{ echo " ";}
+
+
+
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+--  Female Marriage related Info / sb-biodata-7  --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+$sql = "INSERT INTO 7bd_marriage_related_qs_female
+(user_id, guardians_agree, anyjob_aftermarriage, studies_aftermarriage, agree_marriage_student, profileby, profilecreationdate  ) 
+VALUES
+('$id', '$guardians_agree', '$anyjob_aftermarriage', '$studies_aftermarriage', '$agree_marriage_student', '$profileby', DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+if (mysqli_query($conn,$sql))
+{echo " ";}
+
+
+
+
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+--        Religion Details / sb-biodata-8        --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+    $sql = "INSERT INTO 8bd_religion_details
+    (user_id, religion, yourreligion_condition, profilecreationdate  ) 
+    VALUES
+    ('$id', '$religion', '$yourreligion_condition', DATE_FORMAT(NOW(), '%e %M %Y, %h:%i:%s %p'))";
+    if (mysqli_query($conn,$sql))
+	{echo " ";}
+
+
+
+    
+//Update & Store the data to Database
+/*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
+-- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
+--                S  T  A  R  T                  --
+--     Expected Life Partner / sb-biodata-9      --
+--                                               --
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -*/
+
+$sql = "INSERT INTO 9bd_expected_life_partner
+(user_id, partner_religius, partner_district, partner_maritialstatus, partner_age, partner_skintones, partner_height, partner_education, partner_profession, partner_financial, partner_attributes, profilecreationdate  ) 
+VALUES
+('$id', '$partner_religius', '$partner_district', '$partner_maritialstatus', '$partner_age', '$partner_skintones', '$partner_height', '$partner_education', '$partner_profession', '$partner_financial', '$partner_attributes', CURDATE())";
+if (mysqli_query($conn,$sql))
+{
+    echo "Thanks! Successfully Uploaded New Biodata!";
+    header("Location: view_profile.php?id={$id}");
+}
+}
     /*-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
     -- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
     --                   E   N   D                   --
