6e36b8f541500f96801077d5ca2e7711c24bf7be
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-06-03 03:54:19 +0600

    Update education.php

--- education.php
+++ education.php
@@ -112,8 +112,6 @@ $(document).ready(function(){
 	<div class="shosurbari-biodata">
 		<!-- multistep form -->
 		<form action="" method="POST" id="biodataForm">
-
-
 			<!-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
 			-- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
 			--                S  T  A  R  T                  --
@@ -127,53 +125,48 @@ $(document).ready(function(){
 				//getting profile details from db
 				$sql="SELECT * FROM 3bd_secondaryedu_method WHERE user_id = $id";
 				$result = mysqlexec($sql);
-
 				if($result){
-				$row=mysqli_fetch_assoc($result);
-				
-				if($row){
-				$scndry_edu_method=$row['scndry_edu_method'];
+					$row=mysqli_fetch_assoc($result);
+					if($row){
+					$scndry_edu_method=$row['scndry_edu_method'];
+					}
+					if($row){
+					$maxedu_qulfctn=$row['maxedu_qulfctn'];
+					}
+					if($row){
+					$gnrl_mdrs_secondary_pass=$row['gnrl_mdrs_secondary_pass'];
+					}
+					if($row){
+					$gnrl_mdrs_secondary_pass_year=$row['gnrl_mdrs_secondary_pass_year']; 
+					}
+					if($row){
+					$gnrl_mdrs_secondary_end_year=$row['gnrl_mdrs_secondary_end_year'];
+					}
+					if($row){
+					$gnrlmdrs_secondary_running_std=$row['gnrlmdrs_secondary_running_std'];
+					}
 				}
-				if($row){
-				$maxedu_qulfctn=$row['maxedu_qulfctn'];
-				}
-				if($row){
-				$gnrl_mdrs_secondary_pass=$row['gnrl_mdrs_secondary_pass'];
-				}
-				if($row){
-				$gnrl_mdrs_secondary_pass_year=$row['gnrl_mdrs_secondary_pass_year']; 
-				}
-				if($row){
-				$gnrl_mdrs_secondary_end_year=$row['gnrl_mdrs_secondary_end_year'];
-				}
-				if($row){
-				$gnrlmdrs_secondary_running_std=$row['gnrlmdrs_secondary_running_std'];
-				}
-			}
-
 
 
 
 				//getting profile details from db
 				$sql="SELECT * FROM 3bd_kowmi_madrasaedu_method WHERE user_id = $id";
 				$result = mysqlexec($sql);
-
 				if($result){
-				$row=mysqli_fetch_assoc($result);
-
-				if($row){
-				$qawmi_madrasa_hafez=$row['qawmi_madrasa_hafez'];
+					$row=mysqli_fetch_assoc($result);
+					if($row){
+					$qawmi_madrasa_hafez=$row['qawmi_madrasa_hafez'];
+					}
+					if($row){
+					$qawmimadrasa_dawrapass=$row['qawmimadrasa_dawrapass']; 
+					}
+					if($row){
+					$kowmi_dawrapas_year=$row['kowmi_dawrapas_year'];
+					}
+					if($row){
+					$kowmi_current_edu_level=$row['kowmi_current_edu_level'];
+					}
 				}
-				if($row){
-				$qawmimadrasa_dawrapass=$row['qawmimadrasa_dawrapass']; 
-				}
-				if($row){
-				$kowmi_dawrapas_year=$row['kowmi_dawrapas_year'];
-				}
-				if($row){
-				$kowmi_current_edu_level=$row['kowmi_current_edu_level'];
-				}
-			}
 
 
 
@@ -183,79 +176,74 @@ $(document).ready(function(){
 				//getting profile details from db
 				$sql="SELECT * FROM 3bd_higher_secondaryedu_method WHERE user_id = $id";
 				$result = mysqlexec($sql);
-
 				if($result){
-				$row=mysqli_fetch_assoc($result);
-
-				if($row){
-				$higher_secondary_edu_method=$row['higher_secondary_edu_method'];
+					$row=mysqli_fetch_assoc($result);
+					if($row){
+					$higher_secondary_edu_method=$row['higher_secondary_edu_method'];
+					}
+					if($row){
+					$gnrlmdrs_hrsecondary_pass=$row['gnrlmdrs_hrsecondary_pass']; 
+					}
+					if($row){
+					$gnrlmdrs_hrsecondary_pass_year=$row['gnrlmdrs_hrsecondary_pass_year'];
+					}
+					if($row){
+					$gnrlmdrs_hrsecondary_exam_year=$row['gnrlmdrs_hrsecondary_exam_year'];
+					}
+					if($row){
+					$gnrlmdrs_hrsecondary_group=$row['gnrlmdrs_hrsecondary_group'];
+					}
+					if($row){
+					$gnrlmdrs_hrsecondary_rningstd=$row['gnrlmdrs_hrsecondary_rningstd']; 
+					}
+					if($row){
+					$diploma_hrsecondary_pass=$row['diploma_hrsecondary_pass'];
+					}
+					if($row){
+					$diploma_hrsecondary_pass_year=$row['diploma_hrsecondary_pass_year'];
+					}
+					if($row){
+					$diploma_hrsecondary_sub=$row['diploma_hrsecondary_sub'];
+					}
+					if($row){
+					$diploma_hrsecondary_endingyear=$row['diploma_hrsecondary_endingyear']; 
+					}
 				}
-				if($row){
-				$gnrlmdrs_hrsecondary_pass=$row['gnrlmdrs_hrsecondary_pass']; 
-				}
-				if($row){
-				$gnrlmdrs_hrsecondary_pass_year=$row['gnrlmdrs_hrsecondary_pass_year'];
-				}
-				if($row){
-				$gnrlmdrs_hrsecondary_exam_year=$row['gnrlmdrs_hrsecondary_exam_year'];
-				}
-				if($row){
-				$gnrlmdrs_hrsecondary_group=$row['gnrlmdrs_hrsecondary_group'];
-				}
-				if($row){
-				$gnrlmdrs_hrsecondary_rningstd=$row['gnrlmdrs_hrsecondary_rningstd']; 
-				}
-				if($row){
-				$diploma_hrsecondary_pass=$row['diploma_hrsecondary_pass'];
-				}
-				if($row){
-				$diploma_hrsecondary_pass_year=$row['diploma_hrsecondary_pass_year'];
-				}
-				if($row){
-				$diploma_hrsecondary_sub=$row['diploma_hrsecondary_sub'];
-				}
-				if($row){
-				$diploma_hrsecondary_endingyear=$row['diploma_hrsecondary_endingyear']; 
-				}
-			}
 
 
 
 				//getting profile details from db
 				$sql="SELECT * FROM 3bd_universityedu_method WHERE user_id = $id";
 				$result = mysqlexec($sql);
-
 				if($result){
-				$row=mysqli_fetch_assoc($result);
-
-				if($row){
+					$row=mysqli_fetch_assoc($result);
+					if($row){
 					$scndry_edu_method=$row['scndry_edu_method'];
-				}
-				if($row){
+					}
+					if($row){
 					$higher_secondary_edu_method=$row['higher_secondary_edu_method'];
-				}
-
-				if($row){
-				$varsity_edu_method=$row['varsity_edu_method'];
-				}
-				if($row){
-				$uvarsity_pass=$row['uvarsity_pass'];
-				}
-				if($row){
-				$varsity_passing_year=$row['varsity_passing_year'];
-				}
-				if($row){
-				$university_subject=$row['university_subject']; 
-				}
-				if($row){
-				$varsity_ending_year=$row['varsity_ending_year'];
-				}
-				if($row){
-				$uvarsity_name=$row['uvarsity_name'];
-				}
-				if($row){
-				$others_edu_qualification=$row['others_edu_qualification'];
-				}
+					}
+					if($row){
+					$varsity_edu_method=$row['varsity_edu_method'];
+					}
+					if($row){
+					$uvarsity_pass=$row['uvarsity_pass'];
+					}
+					if($row){
+					$varsity_passing_year=$row['varsity_passing_year'];
+					}
+					if($row){
+					$university_subject=$row['university_subject']; 
+					}
+					if($row){
+					$varsity_ending_year=$row['varsity_ending_year'];
+					}
+					if($row){
+					$uvarsity_name=$row['uvarsity_name'];
+					}
+					if($row){
+					$others_edu_qualification=$row['others_edu_qualification'];
+					}
 				}
 			?>
 
@@ -1120,7 +1108,7 @@ $(document).ready(function(){
 					toggleVarsityFields();
 					toggleVarsityPassFields();
 				</script>
-			    <input type="submit"  name="op" class="action-button" id="edit-submit" value="Submit Biodata">
+				<button type="submit" id="edit-submit" name="op" class="biodata-submit"><span></span> Submit</button>			
 			</fieldset>
 			<!--3 fieldsets end-->
 			<!-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
@@ -1130,35 +1118,9 @@ $(document).ready(function(){
 			--                                               --
 			-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
 			-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
-
-
-
 		</form>
-
-
-		
-<!-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
--- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
---                S  T  A  R  T                  --
---       Update & Store the data to Database     --
---                                               --
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
-
-<!-- -- -- -- -- -- -- -- -- -- -- -- -- ---- -- --
--- -- -- -- -- -- -- -- --- -- -- -- -- -- -- -- --
---                   E   N   D                   --
---       Update & Store the data to Database     --
---                                               --
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- ---
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -->
-
-
 	</div>
-
-
 	<?php include_once("footer.php");?>
-
 </body>
 </html>
 
@@ -1170,225 +1132,87 @@ $(document).ready(function(){
 
 
 
-
-
-
-<style>
-	#height-error-message {
-	color: red;
-	font-size: 12px;
-}
-
-/*form styles*/
-.sb-biodata{
-    border: 1px solid #ccc;
-    border-radius: 6px;
-    width: 85%;
-    gap: 1.5rem;
-    padding: 20px;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 40px;
-    margin-top: 20px;
-    background: white;
-   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-}
-
-#biodataForm {
-    width: 80%;
-    margin: 0px auto;
-    text-align: center;
-}
-
-#biodataForm fieldset {
-    background: white;
-    border: 0 none;
-    border-radius: 3px;
-    padding: 10px;
-    box-sizing: border-box;
-    width: 550px;
-    margin: auto;
-}
-
-/*Hide all except first fieldset*/
-#biodataForm fieldset:not(:first-of-type) {
-    display: none;
-}
-
-/*buttons*/
-#biodataForm .action-button {
-    width: 100px;
-    background: #27AE60;
-    font-weight: bold;
-    color: white;
-    border: 0 none;
-    border-radius: 1px;
-    cursor: pointer;
-    padding: 10px 5px;
-    margin: 10px 5px;
-}
-
-#biodataForm .action-button:hover, #biodataForm .action-button:focus {
-    box-shadow: 0 0 0 2px white, 0 0 0 3px #27AE60;
-}
-
-/*progressbar*/
-#progressbar {
-    background: #fff;
-    margin-bottom: 30px;
-    overflow: hidden;
-    /*CSS counters to number the steps*/
-    counter-reset: step;
-    padding: 70px 0px 10px 0px;
-}
-
-#progressbar li {
-    list-style-type: none;
-    color: #000;
-    font-size: 15px;
-    width: 12%;
-    float: left;
-    position: relative;
-    z-index: 2; /* Ensure the list items appear above the connectors */
-}
-
-#progressbar li:before {
-    content: counter(step);
-    counter-increment: step;
-    width: 34px;
-    line-height: 31px;
-    display: block;
-    font-size: 10px;
-    color: #fff;
-    background: #06b6d4;
-    border-radius: 50%;
-    margin: -5px 0px 10px 65px;
-	font-size: 14px;
-}
-
-#progressbar li:after {
-    content: '';
-    width: 100%;
-    height: 4px;
-    border-radius: 0px;
-    background: #06b6d4;
-    position: absolute;
-    left: -20%;
-    top: 9px;
-    z-index: -1; /* Ensure the connectors appear behind the list items */
-}
-
-#progressbar li:first-child:after {
-    /*connector not needed before the first step*/
-    content: none; 
-}
-
-/*marking active/completed steps green*/
-/*The number of the step and the connector before it = green*/
-#progressbar li.active:before,  #progressbar li.active:after{
-    background: #27AE60;
-    color: white;
-}
-
-.error-message-empty{
-	color: red;
-}
-</style>
- 
-
 <script>
-// jQuery time
-var current_fs, next_fs, previous_fs; // fieldsets
+	// jQuery time
+	var current_fs, next_fs, previous_fs; // fieldsets
 
-$(".next").click(function() {
-  current_fs = $(this).closest("fieldset");
-  next_fs = current_fs.next("fieldset");
+	$(".next").click(function() {
+		current_fs = $(this).closest("fieldset");
+		next_fs = current_fs.next("fieldset");
 
-  // Validate fields in the current fieldset
-  if (!validateFields(current_fs)) {
-    return; // Stop execution if fields are empty
-  }
+		// Validate fields in the current fieldset
+		if (!validateFields(current_fs)) {
+			return; // Stop execution if fields are empty
+		}
 
-  // Activate next step on progressbar
-  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+		// Activate next step on progressbar
+		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
-  // Show the next fieldset
-  next_fs.show();
-  // Hide the current fieldset
-  current_fs.hide();
+		// Show the next fieldset
+		next_fs.show();
+		// Hide the current fieldset
+		current_fs.hide();
 
-  // Smooth scroll to the top of the progress bar
-  $('html, body').animate({ scrollTop: $('#progressbar').offset().top }, 800);
-});
-
-
-$(".previous").click(function() {
-  current_fs = $(this).closest("fieldset");
-  previous_fs = current_fs.prev("fieldset");
-
-  // Show the previous fieldset
-  previous_fs.show();
-  // Hide the current fieldset
-  current_fs.hide();
-
-  // De-activate current step on progressbar
-  $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-  // Smooth scroll to the top of the progress bar
-  $('html, body').animate({ scrollTop: $('#progressbar').offset().top }, 800);
-});
+		// Smooth scroll to the top of the progress bar
+		$('html, body').animate({ scrollTop: $('#progressbar').offset().top }, 800);
+	});
 
 
 
+	$(".previous").click(function() {
+		current_fs = $(this).closest("fieldset");
+		previous_fs = current_fs.prev("fieldset");
+
+		// Show the previous fieldset
+		previous_fs.show();
+		// Hide the current fieldset
+		current_fs.hide();
+
+		// De-activate current step on progressbar
+		$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+		// Smooth scroll to the top of the progress bar
+		$('html, body').animate({ scrollTop: $('#progressbar').offset().top }, 800);
+	});
 
 
 
-// Validate the fields in the current fieldset
-function validateFields(current_fs) {
-  var isValid = true;
+	// Validate the fields in the current fieldset
+	function validateFields(current_fs) {
+		var isValid = true;
 
-  // Get all required input fields within the current fieldset
-  var inputs = current_fs.find(":input[required]");
+		// Get all required input fields within the current fieldset
+		var inputs = current_fs.find(":input[required]");
 
-  // Remove previous error messages
-  current_fs.find(".error-message-empty").remove();
+		// Remove previous error messages
+		current_fs.find(".error-message-empty").remove();
 
-  // Loop through each input field and check if it's empty
-  inputs.each(function() {
-    if ($(this).val().trim() === "") {
-      $(this).addClass("error"); // Add error class to highlight the empty field
-      isValid = false;
+		// Loop through each input field and check if it's empty
+		inputs.each(function() {
+			if ($(this).val().trim() === "") {
+			$(this).addClass("error"); // Add error class to highlight the empty field
+			isValid = false;
 
-      // Show error message
-      var errorMessage = "<span class='error-message-empty'>This field is required.</span>";
-      $(this).after(errorMessage);
-    } else {
-      $(this).removeClass("error"); // Remove error class if the field is not empty
-    }
-  });
+			// Show error message
+			var errorMessage = "<span class='error-message-empty'>This field is required.</span>";
+			$(this).after(errorMessage);
+			} else {
+			$(this).removeClass("error"); // Remove error class if the field is not empty
+			}
+		});
 
-  // Scroll to the first empty input field
-  if (!isValid) {
-    var firstEmptyField = current_fs.find(".error").first();
-    var windowHeight = $(window).height();
-    var fieldTop = firstEmptyField.offset().top;
-    var fieldHeight = firstEmptyField.outerHeight();
-    var middleOffset = (windowHeight / 2) - (fieldHeight / 2);
-    var scrollTo = fieldTop - middleOffset;
 
-    $('html, body').animate({ scrollTop: scrollTo }, 800);
-  }
+		// Scroll to the first empty input field
+		if (!isValid) {
+			var firstEmptyField = current_fs.find(".error").first();
+			var windowHeight = $(window).height();
+			var fieldTop = firstEmptyField.offset().top;
+			var fieldHeight = firstEmptyField.outerHeight();
+			var middleOffset = (windowHeight / 2) - (fieldHeight / 2);
+			var scrollTo = fieldTop - middleOffset;
 
-  return isValid;
-}
+			$('html, body').animate({ scrollTop: scrollTo }, 800);
+		}
+		return isValid;
+	}
 </script>
-
-
-
-
-
-
-
-
-
-<!-- Bellow my database code -->
