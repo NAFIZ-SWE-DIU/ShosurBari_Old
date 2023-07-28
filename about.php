8b5ccd2bb98dddccfc7a5f835b01e2d10e760d81
Nafizul Islam <96583087+NAFIZ-SWE-DIU@users.noreply.github.com>
2023-07-28 18:20:10 +0600

    Update about.php

--- about.php
+++ about.php
@@ -4,8 +4,10 @@
 <!DOCTYPE HTML>
 <html>
 
+
 <head>
-<title>About Us - ShosurBari</title>
+<title>About Us | ShosurBari</title>
+<link rel="icon" href="images/shosurbari-icon.png" type="image/png">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
@@ -20,20 +22,6 @@
 <!----font-Awesome----->
 <link href="css/font-awesome.css" rel="stylesheet"> 
 <!----font-Awesome----->
-<script>
-$(document).ready(function(){
-    $(".dropdown").hover(            
-        function() {
-            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-            $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-            $(this).toggleClass('open');       
-        }
-    );
-});
-</script>
 </head>
 
 
@@ -103,113 +91,13 @@ $(document).ready(function(){
 			</p>
 		</div>
 
-
-
-		<div class="about_middle">
-			<h2>Our Team</h2>
-			<div class="about_middle-grid1">
-
-				<div class="col-sm-6 testi_grid list-item-0">
-					<blockquote class="testi_grid_blockquote">
-
-						<figure class="featured-thumbnail">
-							<img src="images/nn.png"  class="img-responsive" alt=""/>
-						</figure>
-
-						<div>
-							<h3>Nafizul Islam Noyon</h3>
-							<h4>CEO and Software Engineer:</h4>
-							<p>
-							Nafizul Islam Noyon is the CEO of Shosurbari.com and a highly skilled software engineer. With a passion for creating innovative solutions,
-							Nafizul has been instrumental in the development and maintenance of our matrimony platform. His technical expertise and visionary 
-							leadership have played a crucial role in shaping the website into what it is today. Nafizul's dedication to providing a seamless
-							user experience has been pivotal in establishing Shosurbari.com as a trusted platform for matrimonial connections.
-							</p>
-							<div class="clearfix"></div>
-						</div>
-
-					</blockquote>
-					<small class="testi-meta"><span class="user">Eiusmod tempor incididunt</span></small>
-				</div>
-
-
-				<div class="col-sm-6 testi_grid list-item-1">
-					<blockquote class="testi_grid_blockquote">
-
-						<figure class="featured-thumbnail">
-							<img src="images/nafi.png" class="img-responsive" alt=""/>
-						</figure>
-
-						<div>
-							<h3>Nasimul Islam Nayem</h3>
-							<h4>Security Consultant and MBBS Doctor:</h4>
-							<p>
-								Nasimul Islam Nayem is a key member of our team, bringing a unique blend of expertise as a security consultant 
-								and an accomplished MBBS doctor. With a deep understanding of online security, Nasimul ensures the safety and
-								privacy of our users' data. His commitment to maintaining the highest security standards sets Shosurbari.com 
-								apart as a secure and reliable platform. Nasimul's background in medicine also provides valuable insights into
-								user safety and well-being, further enhancing the overall user experience.
-							</p>
-							<div class="clearfix"></div>
-						</div>
-
-					</blockquote>
-					<small class="testi-meta1"><span class="user">Sint occaecat </span></small>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-
-
-			<div class="about_middle-grid2">
-					
-				<div class="col-sm-6 testi_grid list-item-0">
-					<blockquote class="testi_grid_blockquote">
-
-						<figure class="featured-thumbnail">
-							<img src="images/nafizn.png" class="img-responsive" alt=""/>
-						</figure>
-
-						<div>
-							<h3>Aive Rahman</h3>
-							<h4>Customer Support Specialist and Civil Engineer:</h4>
-							<p>
-								Aive is an indispensable part of our team, serving as a customer support specialist with a background in civil engineering.
-								With her exceptional communication skills and dedication to customer satisfaction, Aive is always ready to assist users 
-								with any inquiries or concerns they may have. Her ability to empathize and provide prompt solutions ensures that our users
-								receive the support they need, fostering a positive and engaging experience on Shosurbari.com. Aive's technical expertise 
-								as a civil engineer also contributes to enhancing the functionality and usability of our platform.
-							</p>
-							<div class="clearfix"></div>
-						</div>
-
-					</blockquote>
-					<small class="testi-meta"><span class="user">Eiusmod tempor incididunt</span></small>
-				</div>
-
-				<div class="col-sm-6 testi_grid list-item-1">
-					<blockquote class="testi_grid_blockquote">
-
-						<figure class="featured-thumbnail">
-							<img src="images/naf.png" class="img-responsive" alt=""/>
-						</figure>
-
-						<div>
-							<h3>Nafizul Islam Noyon</h3>
-							<h4>Security Consultant and MBBS Doctor:</h4>
-							<p>
-								Aenean nonummy hendrerit mau phasellu porta. Fusce suscipit varius mi sed. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui.…
-							</p>
-							<div class="clearfix"></div>
-						</div>
-
-					</blockquote>
-					<small class="testi-meta1"><span class="user">Sint occaecat </span></small>
-				</div>
-				<div class="clearfix"> </div>
-			</div>
-
-		</div>
 	</div>
+
+	<style>
+	html, body{
+    background:#fff;
+}
+	</style>
 
 	<?php include_once("footer.php");?>
 </body>
